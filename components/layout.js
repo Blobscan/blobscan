34648b15142ca413c2ee465182221cfccfd39567
Matias <mantitaaa89@gmail.com>
2022-10-08 21:08:33 -0300

    ui lastest update

--- components/layout.js
+++ components/layout.js
@@ -1,27 +1,42 @@
-import { Box, Button, Container, Heading, Input, InputGroup, InputRightElement, Text, Link } from "@chakra-ui/react"
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+  Link,
+} from "@chakra-ui/react";
 
-const Layout = ({children}) => (
-<Container minWidth="100%" centerContent>
-<Box maxW="2xl" m="0 auto">
-<Heading as="h1" textAlign="center" fontSize="6xl" mt="100px" mb="50px">
-blobscan
-</Heading>
-<Text fontSize="xl" textAlign="center" mt="30px">
-Blob transaction explorer for <Link href="https://www.eip4844.com/">EIP-4844</Link>
-</Text>
+const Layout = ({ children }) => (
+  <Container minWidth="100%" centerContent>
+    <Box maxW="2xl" m="0 auto">
+      <Heading as="h1" textAlign="center" fontSize="9xl" mt="100px" mb="50px">
+        bl<span>⍥</span>bscan
+        {/* blöbscan */}
+      </Heading>
+      <InputGroup size="lg" mt="30px">
+        <Input
+          width="lg"
+          placeholder="Block hash, Transaction hash, address  or blob hash"
+          variant="filled"
+          _placeholder={{ opacity: 0.3, color: "inherit" }}
+        />
+        <InputRightElement width="4.5rem" mr={"1rem"}>
+          <Button h="1.75rem" size="sm">
+            Search
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+      <Text fontSize="xl" textAlign="center" mt="30px">
+        Blob transactions explorer for{" "}
+        <Link href="https://www.eip4844.com/">EIP-4844</Link>
+      </Text>
+    </Box>
+    {children}
+  </Container>
+);
 
-<InputGroup size='lg' mt="30px">
-<Input width="lg" placeholder='Block hash, Transaction hash, address, or blob hash' />
-<InputRightElement width='4.5rem'>
-<Button h='1.75rem' size='sm' >
-Search
-</Button>
-</InputRightElement>
-</InputGroup>
-</Box>
-{ children }
-</Container>
-
-)
-
-export default Layout
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,48 @@
-import { Box, Heading, Text, Link, Input, InputGroup, Button, InputRightElement, Grid, GridItem, Container } from "@chakra-ui/react";
+import { Box, Link, SimpleGrid } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import Layout from '../components/layout'
+import Layout from "../components/layout";
 import { connectToDatabase } from "../util/mongodb";
 
 const Home: NextPage = ({ blocks }: any) => {
   return (
     <Layout>
-
-<Grid templateColumns='repeat(5, 1fr)' gap={6} mt="30">
-    {blocks?.map((b: any) => {
-      return (
-        <GridItem key={b.hash}>
-          <Box>
-        <Link href={`/block/${b.number}`}>
-        Block #{b.number}
-        </Link>
-        </Box>
-        </GridItem>
-        );
-      })}
-      </Grid>
+      <SimpleGrid columns={{ sm: 2, md: 4, xl: 6 }} gap={6} mt="140">
+        {blocks.map((b: any) => {
+          return (
+            <Box
+              key={b.hash}
+              as="button"
+              borderRadius="md"
+              bg="#502eb4"
+              color="white"
+              px={10}
+              h={20}
+            >
+              <Link href={`/block/${b.number}`}>Block #{b.number}</Link>
+            </Box>
+          );
+        })}
+      </SimpleGrid>
     </Layout>
-    
-    
-      );
+  );
+};
+
+export const getServerSideProps = async () => {
+  try {
+    const { db } = await connectToDatabase();
+    const blocks = await db
+      .collection("blocks")
+      .find({})
+      //   .sort({ metacritic: -1 })
+      .limit(12)
+      .toArray();
+
+    return {
+      props: { blocks: JSON.parse(JSON.stringify(blocks)) },
     };
-    
-    export const getServerSideProps = async () => {
-      try {
-        const { db } = await connectToDatabase();
-        const blocks = await db
-        .collection("blocks")
-        .find({})
-        //   .sort({ metacritic: -1 })
-        .limit(5)
-        .toArray();
-        
-        return {
-          props: { blocks: JSON.parse(JSON.stringify(blocks)) },
-        };
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    
-    export default Home;
-    
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export default Home;
