902750f93614216f82dbf052ed53366daa9c202d
Matias <mantitaaa89@gmail.com>
2022-12-24 00:13:30 -0300

    feat: enable  dark-mode on SwitchStyles, add theme-tool to package.json

--- theme/ui-components/SwitchStyles.ts
+++ theme/ui-components/SwitchStyles.ts
@@ -1,35 +1,37 @@
 import { switchAnatomy } from "@chakra-ui/anatomy";
 import { createMultiStyleConfigHelpers } from "@chakra-ui/react";
 
+import { mode } from "@chakra-ui/theme-tools";
+
 const { definePartsStyle, defineMultiStyleConfig } =
   createMultiStyleConfigHelpers(switchAnatomy.keys);
 
-const baseStyle = definePartsStyle({
-  container: {
-    position: "relative",
-  },
-  thumb: {
-    position: "absolute",
-    top: "-1",
-    left: "-1",
-    color: "success.500",
-    bgColor: "shades.0o0",
-    border: "1px solid",
-    borderColor: "primary.200",
-    _checked: {
-      bgColor: "primary.200",
+const baseStyle = (props) =>
+  definePartsStyle({
+    container: {
+      position: "relative",
     },
-  },
-  track: {
-    border: "none",
-    bgColor: "neutral.300",
-    w: "30px",
-    h: "10px",
-    _checked: {
-      bgColor: "primary.100",
+    thumb: {
+      position: "absolute",
+      top: "-1",
+      left: "-1",
+      bgColor: "shades.0o0",
+      border: "1px solid",
+      borderColor: "primary.200",
+      _checked: {
+        bgColor: "primary.200",
+      },
     },
-  },
-});
+    track: {
+      border: "none",
+      bgColor: mode("neutral.300", "")(props),
+      w: "30px",
+      h: "10px",
+      _checked: {
+        bgColor: mode("", "primary.100")(props),
+      },
+    },
+  });
 
 export const switchTheme = defineMultiStyleConfig({
   baseStyle,
