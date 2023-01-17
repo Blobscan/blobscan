6a18d7e8996d1082e408314376766c09752c8b40
Matias <mantitaaa89@gmail.com>
2023-01-17 20:20:10 -0300

    feat(AppLayoutTopBar): add dark mode

--- components/AppLayout/AppLayoutTopBar.tsx
+++ components/AppLayout/AppLayoutTopBar.tsx
@@ -1,4 +1,9 @@
-import { HStack, Flex, Icon, Button, useMediaQuery } from "@chakra-ui/react";
+import {
+  HStack,
+  Flex,
+  useMediaQuery,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { MdSettings } from "react-icons/md";
 
 import { EnableAccount } from "../Button/EnableAccount";
@@ -10,6 +15,9 @@ export const TopBar = () => {
     ssr: true,
     fallback: false,
   });
+  const bgColor = useColorModeValue("body", "neutral.dark.500");
+  const bordeColor = useColorModeValue("neutral.200", "neutral.dark.400");
+
   return (
     <HStack
       justify={"end"}
@@ -18,7 +26,8 @@ export const TopBar = () => {
       px={["16px", "41px"]}
       mb={["60px", "198px"]}
       borderBottom="1px solid"
-      borderColor={"neutral.200"}
+      borderColor={bordeColor}
+      bgColor={bgColor}
     >
       {isDeskTop ? <DesktopNav /> : <MobileNav />}
     </HStack>
@@ -31,6 +40,7 @@ export const DesktopNav = () => {
       {/* TODO: uncomment this  */}
       {/* <EnableAccount /> */}
       <Switcher />
+      {/* TODO: optimize Icon Setting  */}
       {/* <Icon as={MdSettings} fill="neutral.700" ml="24px" w="17px" h="17px" /> */}
     </Flex>
   );
