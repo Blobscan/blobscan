501cd5fca205e5753699f06b415cac5e48b2d6f7
Matias <mantitaaa89@gmail.com>
2023-01-19 16:10:39 -0300

    fix(Footer): set up dark mode

--- components/AppLayout/Footer.tsx
+++ components/AppLayout/Footer.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { Text, Box, useColorModeValue } from "@chakra-ui/react";
+import { Text, useColorModeValue } from "@chakra-ui/react";
 
 const Footer = () => {
-  const color = useColorModeValue("nuetral.700", "");
+  const color = useColorModeValue("nuetral.700", "neutral.dark.100");
   return (
-    <Box position={"relative"} bottom={"0px"}>
-      <Text
-        align={"center"}
-        py="30px"
-        textStyle={"paragraph.sm"}
-        mt="40px"
-        w="100%"
-        color={color}
-      >
-        Blobscan @ 2022 | v1.0.0
-      </Text>
-    </Box>
+    <Text
+      position={"relative"}
+      bottom={"0px"}
+      align={"center"}
+      py="30px"
+      textStyle={"paragraph.sm"}
+      mt="40px"
+      w="100%"
+      color={color}
+    >
+      Blobscan @ 2022 | v1.0.0
+    </Text>
   );
 };
 
