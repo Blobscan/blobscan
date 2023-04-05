1ea44aa87863200f13e51e0a083637838ffac4c4
0xGabi <gabrielpk.18@gmail.com>
2023-04-05 01:08:00 -0300

    chore: delete files moved to src

--- theme/colors/index.ts
+++
@@ -1,7 +0,0 @@
-import { lightColors } from "./light";
-import { darkColors } from "./dark";
-
-export const colors = {
-  light: lightColors,
-  dark: darkColors,
-};
--- util/helpers.ts
+++
@@ -1,5 +0,0 @@
-export const formatDate = (date: number) => {
-  const newDate = new Date(date * 1000).toUTCString();
-
-  return newDate;
-};
