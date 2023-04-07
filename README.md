3d3ff152905b7a9448899cd5567cc68c84fdcbe6
0xGabi <gabrielpk.18@gmail.com>
2023-04-07 00:04:26 -0300

    chore: remove boilerplate we do not want to use

--- README.md
+++ README.md
@@ -15,16 +15,9 @@ It uses [Turborepo](https://turborepo.org/) and contains:
 .vscode
   └─ Recommended extensions and settings for VSCode users
 apps
-  ├─ expo
-  |   ├─ Expo SDK 48
-  |   ├─ React Native using React 18
-  |   ├─ Navigation using Expo Router
-  |   ├─ Tailwind using Nativewind
-  |   └─ Typesafe API calls using tRPC
   └─ next.js
       ├─ Next.js 13
       ├─ React 18
-      ├─ Tailwind CSS
       └─ E2E Typesafe API Server & Client
 packages
  ├─ api
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,8 +1,7 @@
 /** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig*/
 /** @typedef  {import("prettier").Config} PrettierConfig*/
-/** @typedef  {{ tailwindConfig: string }} TailwindConfig*/
 
-/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+/** @type { PrettierConfig | SortImportsConfig } */
 const config = {
   arrowParens: "always",
   printWidth: 80,
@@ -14,16 +13,14 @@ const config = {
   // pluginSearchDirs: false,
   plugins: [
     "@ianvs/prettier-plugin-sort-imports",
-    "prettier-plugin-tailwindcss",
   ],
-  tailwindConfig: "./packages/config/tailwind",
   importOrder: [
     "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
     "^(next/(.*)$)|^(next$)",
     "^(expo(.*)$)|^(expo$)",
     "<THIRD_PARTY_MODULES>",
     "",
-    "^@acme/(.*)$",
+    "^@blobscan/(.*)$",
     "",
     "^~/utils/(.*)$",
     "^~/components/(.*)$",
