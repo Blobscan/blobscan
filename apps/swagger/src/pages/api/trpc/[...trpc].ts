0117fac1bf3dde92bea8680dcafa58ca7bc26427
PJColombo <paulo.colombo@pm.me>
2023-05-01 03:15:49 +0200

    fix: remove nextjs rest api package

--- apps/swagger/src/pages/api/trpc/[...trpc].ts
+++
@@ -1,9 +0,0 @@
-import { createNextApiHandler } from "@trpc/server/adapters/next";
-
-import { appRouter, createTRPCContext } from "@blobscan/api";
-
-// Handle incoming tRPC requests
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: createTRPCContext,
-});
