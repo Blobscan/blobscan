62c25866cf96724eb3116f68c825b4d5e4845461
0xGabi <gabrielpk.18@gmail.com>
2023-05-04 12:25:33 -0300

    chore(api): jwtAuth all indexer routes

--- packages/api/src/router/indexer.ts
+++ packages/api/src/router/indexer.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-import { createTRPCRouter, jwtAuthedProcedure, publicProcedure } from "../trpc";
+import { createTRPCRouter, jwtAuthedProcedure } from "../trpc";
 
 export const indexerRouter = createTRPCRouter({
   getSlot: jwtAuthedProcedure
@@ -21,7 +21,7 @@ export const indexerRouter = createTRPCRouter({
 
       return { slot: config?.lastSlot ?? 0 };
     }),
-  updateSlot: publicProcedure
+  updateSlot: jwtAuthedProcedure
     .meta({
       openapi: {
         method: "POST",
@@ -47,7 +47,7 @@ export const indexerRouter = createTRPCRouter({
 
       return { slot };
     }),
-  index: publicProcedure
+  index: jwtAuthedProcedure
     .meta({
       openapi: {
         method: "POST",
