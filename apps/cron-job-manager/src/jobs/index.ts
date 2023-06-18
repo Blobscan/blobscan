88c6642ab7ff2a7f12dd5857333bf26a8918d50a
PJColombo <paulo.colombo@pm.me>
2023-06-18 16:29:48 +0200

    feat: rollback internal cron job scheduler in favor of Vercel cron jobs service

--- apps/cron-job-manager/src/jobs/index.ts
+++
@@ -1,7 +0,0 @@
-import type Bree from "bree";
-
-export const jobs: (Bree.JobOptions | string)[] = [
-  "backfill-daily-blob-stats",
-  "backfill-daily-block-stats",
-  "backfill-daily-transaction-stats",
-];
