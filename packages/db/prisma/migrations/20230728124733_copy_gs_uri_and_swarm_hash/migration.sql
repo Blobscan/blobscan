c12f257da2feb933c46363f97eebe40911622a07
PJColombo <paulo.colombo@pm.me>
2023-07-28 19:32:12 +0200

    fix(db): add postgres storage refs to copy migration file

--- packages/db/prisma/migrations/20230728124733_copy_gs_uri_and_swarm_hash/migration.sql
+++ packages/db/prisma/migrations/20230728124733_copy_gs_uri_and_swarm_hash/migration.sql
@@ -7,8 +7,15 @@ INSERT INTO "BlobDataStorageReference"
   FROM "Blob" AS b
   WHERE b."gsUri" IS NOT NULL
   UNION 
-  SELECT b."versionedHash" AS "blobHash",
-  'swarm'::"BlobStorage" AS "blobStorage",
-  b."swarmHash" AS "dataReference"
+  SELECT 
+    b."versionedHash" AS "blobHash",
+    'swarm'::"BlobStorage" AS "blobStorage",
+    b."swarmHash" AS "dataReference"
   FROM "Blob" AS b
-  WHERE b."swarmHash" IS NOT NULL;
+  WHERE b."swarmHash" IS NOT NULL
+  UNION
+  SELECT
+    d."id" AS "blobHash",
+    'postgres'::"BlobStorage" AS "blobStorage",
+    d."id" AS "dataReference"
+  FROM "BlobData" AS d;
