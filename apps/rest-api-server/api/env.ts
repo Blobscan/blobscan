af2d492fe56c4cc120d772a56f0c07f2d3aebbef
PJColombo <paulo.colombo@pm.me>
2023-08-05 18:20:26 +0200

    chore(telemetry): build auth headers manually

--- apps/rest-api-server/api/env.ts
+++ apps/rest-api-server/api/env.ts
@@ -7,6 +7,22 @@ const envSchema = createEnvSchema({
     schema: z.coerce.number().int().positive(),
     default: 3001,
   },
+  OTEL_EXPORTER_OTLP_PROTOCOL: {
+    schema: z.enum(["grpc", "http/protobuf", "http/json"]),
+    optional: true,
+  },
+  OTEL_EXPORTER_OTLP_ENDPOINT: {
+    schema: z.string().url(),
+    optional: true,
+  },
+  GRAFANA_INSTANCE_ID: {
+    schema: z.coerce.number().int().positive(),
+    optional: true,
+  },
+  GRAFANA_TOKEN: {
+    schema: z.string(),
+    optional: true,
+  },
 });
 
 export const env = envSchema.parse(process.env);
--- apps/rest-api-server/api/telemetry.ts
+++ apps/rest-api-server/api/telemetry.ts
@@ -1,15 +1,35 @@
+import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import { Resource } from "@opentelemetry/resources";
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 
+import { env } from "./env";
+
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
+const exporterOptions: { headers?: Record<string, string> } = {};
+
+/**
+ * The environment variable 'OTEL_EXPORTER_OTLP_HEADERS' is not set directly. The OTLP Exporter does not
+ * correctly parse authentication keys containing characters such as `=` which it interprets as parse separators.
+ * Therefore, to avoid parsing errors, we build the auth header ourselves.
+ */
+if (env.GRAFANA_INSTANCE_ID && env.GRAFANA_TOKEN) {
+  exporterOptions.headers = {
+    Authorization: `Basic ${btoa(
+      `${env.GRAFANA_INSTANCE_ID}:${env.GRAFANA_TOKEN}`
+    )}`,
+  };
+}
+
 const sdk = new NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: "blobscan_rest_api",
     [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: process.env.NODE_ENV,
   }),
-  traceExporter: new OTLPTraceExporter(),
+  traceExporter: new OTLPTraceExporter(exporterOptions),
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
