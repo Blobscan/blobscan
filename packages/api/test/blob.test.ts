4e407266aa36cb74bd292eab9a13585e406eebc1
0xGabi <gabrielpk.18@gmail.com>
2023-08-30 01:23:33 -0300

    feat(test): @blobscan/api: add test for blob router

---
+++ packages/api/test/blob.test.ts
@@ -0,0 +1,96 @@
+import type { inferProcedureInput } from "@trpc/server";
+import { TRPCError } from "@trpc/server";
+import { describe, expect, it, vi } from "vitest";
+
+import type { TRPCContext } from "../src/context";
+import { createTRPCInnerContext } from "../src/context";
+import { appRouter } from "../src/root";
+import type { AppRouter } from "../src/root";
+
+vi.mock("@blobscan/blob-storage-manager/src/env", () => ({
+  env: {
+    POSTGRES_STORAGE_ENABLED: true,
+    GOOGLE_STORAGE_ENABLED: true,
+    GOOGLE_STORAGE_PROJECT_ID: "blobscan",
+    GOOGLE_STORAGE_BUCKET_NAME: "blobscan-test",
+    GOOGLE_STORAGE_API_ENDPOINT: "http://localhost:8080",
+  },
+}));
+
+describe("Blob router", async () => {
+  const ctx = (await createTRPCInnerContext()) as TRPCContext;
+  const caller = appRouter.createCaller(ctx);
+
+  describe("getAll", () => {
+    it("should get all", async () => {
+      const result = await caller.blob.getAll({});
+      expect(result).toMatchSnapshot();
+    });
+
+    it("should get all with pagination", async () => {
+      type Input = inferProcedureInput<AppRouter["blob"]["getAll"]>;
+      const input: Input = {
+        p: 2,
+        ps: 2,
+      };
+
+      const result = await caller.blob.getAll(input);
+      expect(result).toMatchSnapshot();
+    });
+  });
+
+  describe("getByVersionedHash", () => {
+    it("should get by versioned hash", async () => {
+      type Input = inferProcedureInput<AppRouter["blob"]["getByVersionedHash"]>;
+      const input: Input = {
+        versionedHash: "blobHash004",
+      };
+
+      const result = await caller.blob.getByVersionedHash(input);
+      expect(result).toMatchSnapshot();
+    });
+
+    it("should get by versioned hash from POSTGRES", async () => {
+      type Input = inferProcedureInput<AppRouter["blob"]["getByVersionedHash"]>;
+      const input: Input = {
+        versionedHash: "blobHash005",
+      };
+
+      const result = await caller.blob.getByVersionedHash(input);
+      expect(result).toMatchSnapshot();
+    });
+
+    it("should get by versioned hash from GOOGLE", async () => {
+      type Input = inferProcedureInput<AppRouter["blob"]["getByVersionedHash"]>;
+      const input: Input = {
+        versionedHash: "blobHash001",
+      };
+
+      const result = await caller.blob.getByVersionedHash(input);
+      expect(result).toMatchSnapshot();
+    });
+
+    it("should error if no blob with hash", async () => {
+      await expect(
+        caller.blob.getByVersionedHash({
+          versionedHash: "nonExistingHash",
+        })
+      ).rejects.toThrow(
+        new TRPCError({
+          code: "NOT_FOUND",
+          message: `No blob with hash nonExistingHash found`,
+        })
+      );
+    });
+
+    it("should error if no blob data found", async () => {
+      await expect(
+        caller.blob.getByVersionedHash({
+          versionedHash: "blobHash006",
+        })
+      ).rejects.toThrow(
+        new Error("Failed to get blob from any of the storages: ")
+      );
+    });
+  });
+});
