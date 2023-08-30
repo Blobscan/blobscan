0d4b64df9cdd85fed66f8cbe9732d70f7ab22ef0
0xGabi <gabrielpk.18@gmail.com>
2023-08-30 20:05:39 -0300

    feat(test): @blobscan/api: add tests for indexer route

--- packages/api/test/helpers/index.ts
+++ packages/api/test/helpers/index.ts
@@ -1,8 +1,12 @@
-import type { NodeHTTPRequest } from "@trpc/server/adapters/node-http";
+import type {
+  NodeHTTPRequest,
+  NodeHTTPResponse,
+} from "@trpc/server/adapters/node-http";
 import jwt from "jsonwebtoken";
 
 import type { TRPCContext } from "../../src/context";
-import { createTRPCInnerContext, getJWTFromRequest } from "../../src/context";
+import { createTRPCContext } from "../../src/context";
+import { createTRPCInnerContext } from "../../src/context";
 import { appRouter } from "../../src/root";
 
 export async function getCaller({
@@ -13,10 +17,10 @@ export async function getCaller({
     const req = {
       headers: { authorization: `Bearer ${token}` },
     } as NodeHTTPRequest;
-    const apiClient = getJWTFromRequest(req);
 
-    const ctx = (await createTRPCInnerContext({
-      apiClient: apiClient,
+    const ctx = (await createTRPCContext({
+      req,
+      res: {} as NodeHTTPResponse,
     })) as TRPCContext;
 
     return appRouter.createCaller(ctx);
@@ -25,3 +29,63 @@ export async function getCaller({
   const ctx = (await createTRPCInnerContext()) as TRPCContext;
   return appRouter.createCaller(ctx);
 }
+
+export const INDEXER_DATA = {
+  block: {
+    number: 1003,
+    hash: "blockHash003",
+    timestamp: 1630257174,
+    slot: 103,
+    blobGasUsed: "10000",
+    excessBlobGas: "5000",
+  },
+  transactions: [
+    {
+      hash: "txHash008",
+      from: "address9",
+      to: "address3",
+      blockNumber: 1003,
+      gasPrice: "20000",
+      maxFeePerBlobGas: "15000",
+    },
+    {
+      hash: "txHash009",
+      from: "address4",
+      to: "address8",
+      blockNumber: 1003,
+      gasPrice: "10000",
+      maxFeePerBlobGas: "1800",
+    },
+    {
+      hash: "txHash010",
+      from: "address7",
+      to: "address2",
+      blockNumber: 1003,
+      gasPrice: "3000000",
+      maxFeePerBlobGas: "20000",
+    },
+  ],
+  blobs: [
+    {
+      versionedHash: "blobHash008",
+      commitment: "commitment008",
+      data: "0x34567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+      txHash: "txHash008",
+      index: 0,
+    },
+    {
+      versionedHash: "blobHash009",
+      commitment: "commitment009",
+      data: "0x34567890abcdef1234567890abcdef",
+      txHash: "txHash009",
+      index: 0,
+    },
+    {
+      versionedHash: "blobHash010",
+      commitment: "commitment010",
+      data: "0x34567890abcdef1234567890abcdef1234567890abcdef",
+      txHash: "txHash010",
+      index: 0,
+    },
+  ],
+};
---
+++ packages/api/test/indexer.test.ts
@@ -0,0 +1,142 @@
+import { TRPCError } from "@trpc/server";
+import type { inferProcedureInput } from "@trpc/server";
+import exp from "constants";
+import { describe, expect, it, vi } from "vitest";
+
+import { prisma } from "@blobscan/db";
+
+import type { AppRouter } from "../src/root";
+import { getCaller, INDEXER_DATA } from "./helpers";
+
+vi.mock("../src/env", () => ({
+  env: {
+    SECRET_KEY: "supersecret",
+  },
+}));
+
+vi.mock("@blobscan/blob-storage-manager/src/env", () => ({
+  env: {
+    CHAIN_ID: 1,
+    POSTGRES_STORAGE_ENABLED: true,
+    GOOGLE_STORAGE_ENABLED: true,
+    GOOGLE_STORAGE_PROJECT_ID: "blobscan",
+    GOOGLE_STORAGE_BUCKET_NAME: "blobscan-test",
+    GOOGLE_STORAGE_API_ENDPOINT: "http://localhost:8080",
+  },
+}));
+
+describe("Indexer route", async () => {
+  const caller = await getCaller();
+  const callerWithClient = await getCaller({ withClient: true });
+
+  describe("getSlot", () => {
+    it("should return the current slot", async () => {
+      const result = await caller.indexer.getSlot();
+
+      expect(result).toMatchObject({ slot: 101 });
+    });
+  });
+
+  describe("updateSlot", () => {
+    it("should not update the slot if not auth", async () => {
+      type Input = inferProcedureInput<AppRouter["indexer"]["updateSlot"]>;
+      const input: Input = {
+        slot: 110,
+      };
+
+      await expect(caller.indexer.updateSlot(input)).rejects.toThrow(
+        new TRPCError({ code: "UNAUTHORIZED" })
+      );
+    });
+
+    it("should update the slot if auth", async () => {
+      type Input = inferProcedureInput<AppRouter["indexer"]["updateSlot"]>;
+      const input: Input = {
+        slot: 110,
+      };
+
+      await callerWithClient.indexer.updateSlot(input);
+
+      const result = await caller.indexer.getSlot();
+
+      expect(result).toMatchObject({ slot: 110 });
+    });
+  });
+
+  describe("indexData", () => {
+    it("should index new data if auth", async () => {
+      type Input = inferProcedureInput<AppRouter["indexer"]["indexData"]>;
+      const input: Input = INDEXER_DATA;
+
+      await callerWithClient.indexer.indexData(input);
+
+      const block = await caller.block.getByBlockNumber({
+        number: 1003,
+      });
+      expect(block).toMatchSnapshot();
+
+      const addresses = await prisma.address.findMany({
+        select: {
+          address: true,
+        },
+        orderBy: {
+          address: "asc",
+        },
+      });
+      expect(addresses).toMatchSnapshot();
+      expect(addresses).toHaveLength(9);
+
+      const txs = await prisma.transaction.findMany({
+        select: {
+          hash: true,
+          blockNumber: true,
+        },
+        orderBy: {
+          hash: "asc",
+        },
+      });
+      expect(txs).toMatchSnapshot();
+      expect(txs).toHaveLength(9);
+
+      const blobs = await prisma.blob.findMany({
+        select: {
+          versionedHash: true,
+          commitment: true,
+          size: true,
+          firstBlockNumber: true,
+        },
+        orderBy: {
+          versionedHash: "asc",
+        },
+      });
+      expect(blobs).toMatchSnapshot();
+      expect(blobs).toHaveLength(9);
+
+      const storageRefs = await prisma.blobDataStorageReference.findMany({
+        select: {
+          blobHash: true,
+          blobStorage: true,
+          dataReference: true,
+        },
+        orderBy: {
+          blobHash: "asc",
+        },
+      });
+      expect(storageRefs).toMatchSnapshot();
+      expect(storageRefs).toHaveLength(14);
+
+      const blobsOnTransactions = await prisma.blobsOnTransactions.findMany({
+        select: {
+          blobHash: true,
+          txHash: true,
+          index: true,
+        },
+        orderBy: {
+          blobHash: "asc",
+        },
+      });
+      expect(blobsOnTransactions).toMatchSnapshot();
+      expect(blobsOnTransactions).toHaveLength(9);
+    });
+  });
+});
