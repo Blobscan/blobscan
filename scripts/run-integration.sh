a9b26e5e059f140e95f395687e35a4e94fe85e57
0xGabi <gabrielpk.18@gmail.com>
2023-08-31 02:02:40 -0300

    feat(test): configure vitest workspace and simplify test task

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -6,9 +6,3 @@ docker-compose -f $DIR/../docker-compose.yml up -d postgres storage
 echo '🟡 - Waiting for database to be ready...'
 $DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
 npx prisma migrate dev --skip-seed --name init
-if [ "$#" -eq  "0" ]
-  then
-    vitest -c ./vitest.config.integration.ts
-else
-    vitest -c ./vitest.config.integration.ts --ui
-fi
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,9 +5,13 @@ export default defineConfig({
     include: ["test/**/*.test.ts"],
     setupFiles: ["@blobscan/test/src/helpers/setup.ts"],
     threads: false,
+    coverage: {
+      enabled: true,
+      reporter: ["html"],
+    },
     onConsoleLog(log) {
       if (log.includes("prisma:query")) return false;
-      if (log.includes("Current logger will be overwritten")) return false;
+      if (log.includes("Current logger will")) return false;
       if (log.includes("warn(prisma-client)")) return false;
       if (log.includes("<empty line>")) return false;
     },
