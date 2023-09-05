157fd5feffab512d013e1c98f86fc0f14964323c
Pablo Castellano <pablo@anche.no>
2023-09-05 12:39:37 +0200

    fix: use api.blobscan.com base url in openapi spec

--- apps/rest-api-server/api/openapi.ts
+++ apps/rest-api-server/api/openapi.ts
@@ -2,14 +2,12 @@ import { generateOpenApiDocument } from "trpc-openapi";
 
 import { appRouter } from "@blobscan/api";
 
-import { env } from "./env";
-
 // Generate OpenAPI schema document
 export const openApiDocument = generateOpenApiDocument(appRouter, {
   title: "Blobscan API",
   description: "OpenAPI compliant REST API built using tRPC with Express",
   version: "0.0.1",
-  baseUrl: `http://localhost:${env.BLOBSCAN_API_PORT}/api`,
+  baseUrl: `https://api.blobscan.com/api`,
   docsUrl: "https://docs.blobscan.com/",
   tags: ["auth", "blocks", "blobs", "transactions"],
 });
