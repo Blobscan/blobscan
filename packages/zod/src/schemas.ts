da24901d73b9b66c0d8f888e7ff42fce1c7e3908
Pablo Castellano <pablo@anche.no>
2023-09-06 14:14:55 +0200

    Bring booleanSchema back (#174)
    * Bring booleanSchema back
    
    * set defaults

--- packages/zod/src/schemas.ts
+++ packages/zod/src/schemas.ts
@@ -14,4 +14,12 @@ function defaultTransformer(defaultValue: unknown) {
   };
 }
 
+// We use this workaround instead of z.coerce.boolean.default(false)
+// because it considers as "true" any value different than "false"
+// (including the empty string).
+export const booleanSchema = z
+  .string()
+  .refine((s) => s === "true" || s === "false")
+  .transform((s) => s === "true");
+
 export const toBigIntSchema = z.string().transform((value) => BigInt(value));
