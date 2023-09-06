73d131b91a8418474bace5707457086dac15c051
Pablo Castellano <pablo@anche.no>
2023-09-06 15:48:44 +0200

    Set defaults for OTEL_EXPORTER_OTLP_ENDPOINT and OTEL_EXPORTER_OTLP_ENDPOINT (#175)

--- packages/open-telemetry/src/env.ts
+++ packages/open-telemetry/src/env.ts
@@ -4,8 +4,8 @@ export const env = createEnv({
   server: {
     OTEL_EXPORTER_OTLP_PROTOCOL: z
       .enum(["grpc", "http/protobuf", "http/json"])
-      .optional(),
-    OTEL_EXPORTER_OTLP_ENDPOINT: z.string().url().optional(),
+      .default("http/protobuf"),
+    OTEL_EXPORTER_OTLP_ENDPOINT: z.string().url().default("http://localhost:4318"),
     OTLP_AUTH_USERNAME: z.coerce.string().optional(),
     OTLP_AUTH_PASSWORD: z.string().optional(),
     NODE_ENV: z.enum(["development", "test", "production"]).optional(),
