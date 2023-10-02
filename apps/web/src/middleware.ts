4cf7c158c4e937ff343af7de4150e53889f7d9ec
0xGabi <gabrielpk.18@gmail.com>
2023-10-02 20:55:12 -0300

    feat(web): add ratelimit support

---
+++ apps/web/src/middleware.ts
@@ -0,0 +1,22 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { kv } from "@vercel/kv";
+
+const ratelimit = new Ratelimit({
+  redis: kv,
+  limiter: Ratelimit.slidingWindow(2, "10 s"),
+});
+
+// Routes to rate limit
+export const config = {
+  matcher: "/feedback",
+};
+
+export default async function middleware(request: NextRequest) {
+  const ip = request.ip ?? "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
+  return success
+    ? NextResponse.next()
+    : NextResponse.redirect(new URL("/blocked", request.url));
+}
