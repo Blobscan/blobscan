a2ad531c99c4a167988c6a4f9912eecaf1a6f560
PJColombo <paulo.colombo@pm.me>
2023-10-19 16:05:09 +0200

    fix(api): remove prometheus metric counter update logic from procedure handler

--- packages/api/src/routers/tx/getByHash.ts
+++ packages/api/src/routers/tx/getByHash.ts
@@ -1,5 +1,3 @@
-import { ValueType } from "@opentelemetry/api";
-import { MeterProvider } from "@opentelemetry/sdk-metrics";
 import { TRPCError } from "@trpc/server";
 
 import { publicProcedure } from "../../procedures";
@@ -20,18 +18,6 @@ export const getByHash = publicProcedure
         message: `No tx with hash '${hash}'`,
       });
     }
-
-    const meter = new MeterProvider().getMeter("blobscan-api");
-    const counter = meter.createCounter("blobscan_api_requests_total", {
-      valueType: ValueType.INT,
-      description:
-        "Number of requests made to Blobscan API by endpoint, status_code and method",
-    });
-    counter.add(1, {
-      method: "GET",
-      endpoint: "/api/tx/by-hash",
-      status_code: "200",
-    });
 
     return tx;
   });
