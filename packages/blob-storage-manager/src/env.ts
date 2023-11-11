be4a5fcded954aab7c0c675abf0090d097828ef4
PJColombo <paulo.colombo@pm.me>
2023-11-12 01:43:36 +0300

    feat(blob-storage-manager): allow to set a main blob storage

--- packages/blob-storage-manager/src/env.ts
+++ packages/blob-storage-manager/src/env.ts
@@ -1,3 +1,4 @@
+import type { BlobStorage } from "@blobscan/db";
 import { z, booleanSchema, createEnv, presetEnvOptions } from "@blobscan/zod";
 
 export const env = createEnv({
@@ -13,6 +14,7 @@ export const env = createEnv({
       GOOGLE_STORAGE_ENABLED: booleanSchema.default("false"),
       POSTGRES_STORAGE_ENABLED: booleanSchema.default("true"),
       SWARM_STORAGE_ENABLED: booleanSchema.default("false"),
+      MAIN_STORAGE: z.enum(["POSTGRES", "SWARM", "GOOGLE"]).optional(),
     },
 
     ...presetEnvOptions,
@@ -35,5 +37,24 @@ export const env = createEnv({
     }
   },
 });
+
+const enabledStorages: BlobStorage[] = [];
+
+if (env.GOOGLE_STORAGE_ENABLED) {
+  enabledStorages.push("GOOGLE");
+}
+
+if (env.POSTGRES_STORAGE_ENABLED) {
+  enabledStorages.push("POSTGRES");
+}
+
+if (env.SWARM_STORAGE_ENABLED) {
+  enabledStorages.push("SWARM");
+}
+
+export const MAIN_STORAGE =
+  env.MAIN_STORAGE ??
+  // Fallback to one of the enabled storages if none was defined as the main one
+  enabledStorages[0];
 
 export type Environment = typeof env;
--- packages/blob-storage-manager/src/index.ts
+++ packages/blob-storage-manager/src/index.ts
@@ -1,33 +1,8 @@
-import { BlobStorageManager } from "./BlobStorageManager";
-import { env } from "./env";
-import { GoogleStorage, PostgresStorage, SwarmStorage } from "./storages";
-
-let blobStorageManager: BlobStorageManager | undefined;
-
-async function createOrLoadBlobStorageManager(): Promise<BlobStorageManager> {
-  if (!blobStorageManager) {
-    const googleStorage = await GoogleStorage.tryCreateFromEnv(env);
-    const swarmStorage = await SwarmStorage.tryCreateFromEnv(env);
-    const postgresStorage = await PostgresStorage.tryCreateFromEnv(env);
-
-    blobStorageManager = new BlobStorageManager(
-      {
-        GOOGLE: googleStorage,
-        SWARM: swarmStorage,
-        POSTGRES: postgresStorage,
-      },
-      env.CHAIN_ID
-    );
-  }
-
-  return blobStorageManager;
-}
-
-export { env } from "./env";
-export type { BlobReference } from "./BlobStorageManager";
-export {
-  createOrLoadBlobStorageManager,
-  GoogleStorage,
-  SwarmStorage,
-  PostgresStorage,
-};
+export { env, MAIN_STORAGE } from "./env";
+export type {
+  BlobReference,
+  BlobStorageManager,
+  StoreOptions,
+} from "./BlobStorageManager";
+export { createOrLoadBlobStorageManager } from "./blob-storage-manager";
+export { GoogleStorage, PostgresStorage, SwarmStorage } from "./storages";
