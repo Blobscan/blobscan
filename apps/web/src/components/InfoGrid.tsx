c35e9099e909bf89ce5635eb09215c9003410ba2
PJColombo <paulo.colombo@pm.me>
2023-12-25 15:43:45 +0100

    fix(web): polish details skeletons

--- apps/web/src/components/InfoGrid.tsx
+++ apps/web/src/components/InfoGrid.tsx
@@ -4,21 +4,30 @@ import type { ReactNode } from "react";
 import "react-loading-skeleton/dist/skeleton.css";
 import Skeleton from "react-loading-skeleton";
 
+import { useBreakpoint } from "~/hooks/useBreakpoint";
+
 export type InfoGridProps = {
   fields?: { name: ReactNode; value: ReactNode }[];
 };
 
 export const InfoGrid: React.FC<InfoGridProps> = function ({ fields }) {
+  const breakpoint = useBreakpoint();
+  const isCompact = breakpoint === "default";
+  const headerWidth = isCompact ? "100%" : "55%";
+  const valueWidth = isCompact ? "100%" : "70%";
+  const skeletonsLength = isCompact ? 4 : 7;
+  const skeletonHeight = isCompact ? 17 : 20;
+
   return (
     <div className="grid w-full gap-3 md:grid-cols-4">
       {!fields
-        ? Array.from({ length: 4 }).map((_, i) => (
+        ? Array.from({ length: skeletonsLength }).map((_, i) => (
             <Fragment key={i}>
               <div>
-                <Skeleton width={180} />
+                <Skeleton width={headerWidth} height={skeletonHeight} />
               </div>
               <div className="col-span-3">
-                <Skeleton width={400} />
+                <Skeleton width={valueWidth} height={skeletonHeight} />
               </div>
             </Fragment>
           ))
---
+++ apps/web/src/hooks/useBreakpoint.ts
@@ -0,0 +1,44 @@
+import { useEffect, useRef, useState } from "react";
+
+export type Breakpoint = "2xl" | "xl" | "lg" | "md" | "sm" | "default";
+
+function getActiveBreakpoint(): Breakpoint {
+  if (window.matchMedia("(min-width: 1536px)").matches) {
+    return "2xl";
+  } else if (window.matchMedia("(min-width: 1280px)").matches) {
+    return "xl";
+  } else if (window.matchMedia("(min-width: 1024px)").matches) {
+    return "lg";
+  } else if (window.matchMedia("(min-width: 768px)").matches) {
+    return "md";
+  } else if (window.matchMedia("(min-width: 640px)").matches) {
+    return "sm";
+  }
+
+  return "default";
+}
+
+export function useBreakpoint() {
+  const [currentBreakpoint, setCurrentBreakpoint] = useState<Breakpoint>(
+    getActiveBreakpoint()
+  );
+  const breakpointRef = useRef<Breakpoint>();
+
+  useEffect(() => {
+    function trackResize() {
+      breakpointRef.current = getActiveBreakpoint();
+      if (breakpointRef.current !== currentBreakpoint) {
+        setCurrentBreakpoint(breakpointRef.current);
+      }
+    }
+
+    window.addEventListener("resize", trackResize);
+
+    return () => {
+      console.log("going out ");
+      window.removeEventListener("resize", trackResize);
+    };
+  });
+
+  return currentBreakpoint;
+}
