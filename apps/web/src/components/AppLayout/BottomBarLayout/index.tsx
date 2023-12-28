eb4c821bf64ed9e5d53db6aa11aa0c2ea79eb15a
PJColombo <paulo.colombo@pm.me>
2023-12-28 15:03:40 +0100

    fix(web): split footer text  into two lines for improved readability

--- apps/web/src/components/AppLayout/BottomBarLayout/index.tsx
+++ apps/web/src/components/AppLayout/BottomBarLayout/index.tsx
@@ -39,18 +39,17 @@ export const BottomBarLayout = () => {
         </div>
         <div className="max-w-lg text-center text-xs text-contentTertiary-light dark:text-contentTertiary-dark">
           Blobscan is the first open-source block explorer for the{" "}
-          <Link href="https://www.eip4844.com/" isExternal>
-            EIP-4844
-          </Link>{" "}
-          shard blob transactions, providing the necessary infrastructure to
-          scale Ethereum.
+          <Link href="https://www.eip4844.com/">EIP-4844</Link> shard blob
+          transactions, providing the necessary infrastructure to scale
+          Ethereum.
         </div>
-        <span className="text-sm text-contentTertiary-light dark:text-contentTertiary-dark">
-          Blobscan © 2023 · Made with ❤️ by{" "}
-          <Link href="https://blossom.software/" isExternal>
-            Blossom Labs
-          </Link>
-        </span>
+        <div className="text-xs text-contentTertiary-light dark:text-contentTertiary-dark">
+          Made with ❤️ by{" "}
+          <Link href="https://blossom.software/">Blossom Labs</Link>.
+        </div>
+        <div className="text-sm text-contentTertiary-light dark:text-contentTertiary-dark">
+          Blobscan © 2023
+        </div>
       </div>
     </div>
   );
