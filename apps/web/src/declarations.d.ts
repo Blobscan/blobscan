95a8fc51fc7b07d88fd60ec6ddf47600c06c3065
Gabi <gabrielpk.18@gmail.com>
2024-01-20 18:27:08 -0300

    fix(ci): use turbo arbitrary workspace declaration

--- apps/web/src/declarations.d.ts
+++ apps/web/src/declarations.d.ts
@@ -1,3 +1,5 @@
+// We declare svg module to prevent:
+// https://stackoverflow.com/questions/44717164/unable-to-import-svg-files-in-typescript
 declare module "*.svg" {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
