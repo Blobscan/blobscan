35eab4068aa9dc6d36ce7712978375865a977bf9
PJColombo <paulo.colombo@pm.me>
2024-01-28 09:07:08 +0100

    feat(api): add reorg handling procedure

--- packages/api/src/routers/indexer/index.ts
+++ packages/api/src/routers/indexer/index.ts
@@ -1,10 +1,12 @@
 import { t } from "../../trpc-client";
 import { getSlot } from "./getSlot";
+import { handleReorgedSlot } from "./handleReorgedSlot";
 import { indexData } from "./indexData";
 import { updateSlot } from "./updateSlot";
 
 export const indexerRouter = t.router({
   getSlot,
+  handleReorgedSlot,
   indexData,
   updateSlot,
 });
