6d4b4d6d9fa59ef09d7443d6a654a2507703f8d5
PJColombo <paulo.colombo@pm.me>
2024-02-07 16:43:20 +0100

    fix(db): use snake case for transaction fork table

--- packages/db/prisma/migrations/20240125063335_create_transaction_fork_model/migration.sql
+++ packages/db/prisma/migrations/20240125063335_create_transaction_fork_model/migration.sql
@@ -1,15 +1,15 @@
 -- CreateTable
-CREATE TABLE "TransactionFork" (
+CREATE TABLE "transaction_fork" (
     "hash" TEXT NOT NULL,
     "block_hash" TEXT NOT NULL,
     "inserted_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "TransactionFork_pkey" PRIMARY KEY ("hash","block_hash")
+    CONSTRAINT "transaction_fork_pkey" PRIMARY KEY ("hash","block_hash")
 );
 
 -- AddForeignKey
-ALTER TABLE "TransactionFork" ADD CONSTRAINT "TransactionFork_block_hash_fkey" FOREIGN KEY ("block_hash") REFERENCES "block"("hash") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "transaction_fork" ADD CONSTRAINT "transaction_fork_block_hash_fkey" FOREIGN KEY ("block_hash") REFERENCES "block"("hash") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "TransactionFork" ADD CONSTRAINT "TransactionFork_hash_fkey" FOREIGN KEY ("hash") REFERENCES "transaction"("hash") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "transaction_fork" ADD CONSTRAINT "transaction_fork_hash_fkey" FOREIGN KEY ("hash") REFERENCES "transaction"("hash") ON DELETE RESTRICT ON UPDATE CASCADE;
