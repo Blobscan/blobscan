85011e4622509c1fad0c7224cb54076465ecac0c
PJColombo <paulo.colombo@pm.me>
2024-04-14 17:29:18 +0200

    chore(web): fix svgr props inference

--- apps/web/src/declarations.d.ts
+++ apps/web/src/declarations.d.ts
@@ -1,6 +1,8 @@
 // We declare svg module to prevent:
+
 // https://stackoverflow.com/questions/44717164/unable-to-import-svg-files-in-typescript
 declare module "*.svg" {
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  import type { FC, SVGProps } from "react";
+  const content: FC<SVGProps<SVGElement>>;
   export default content;
 }
