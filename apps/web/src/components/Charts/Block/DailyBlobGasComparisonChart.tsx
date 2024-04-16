76b83e934ee0686875887a161d7f119f139c7d8d
PJColombo <paulo.colombo@pm.me>
2024-04-16 16:21:33 +0200

    fix(web): use lighter red color for light mode in blob gas comparison chart

--- apps/web/src/components/Charts/Block/DailyBlobGasComparisonChart.tsx
+++ apps/web/src/components/Charts/Block/DailyBlobGasComparisonChart.tsx
@@ -1,6 +1,7 @@
 import type { FC } from "react";
 import * as echarts from "echarts";
 import type { EChartOption } from "echarts";
+import { useTheme } from "next-themes";
 
 import { ChartCard } from "~/components/Cards/ChartCard";
 import type { DailyBlockStats } from "~/types";
@@ -15,6 +16,7 @@ export type DailyBlobGasComparisonChartProps = Partial<{
 
 export const DailyBlobGasComparisonChart: FC<DailyBlobGasComparisonChartProps> =
   function ({ blobAsCalldataGasUsed, blobGasUsed, days, opts = {} }) {
+    const { resolvedTheme } = useTheme();
     const options: EChartOption<EChartOption.Series> = {
       ...buildTimeSeriesOptions({
         dates: days,
@@ -28,6 +30,7 @@ export const DailyBlobGasComparisonChart: FC<DailyBlobGasComparisonChartProps> =
           data: blobGasUsed,
           stack: "gas",
           type: "bar",
+
           emphasis: {
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
@@ -52,7 +55,7 @@ export const DailyBlobGasComparisonChart: FC<DailyBlobGasComparisonChartProps> =
           stack: "gas",
           type: "bar",
           itemStyle: {
-            color: "#743737",
+            color: resolvedTheme === "dark" ? "#9c3932" : "#e97979",
           },
 
           emphasis: {
@@ -68,7 +71,7 @@ export const DailyBlobGasComparisonChart: FC<DailyBlobGasComparisonChartProps> =
               },
               {
                 offset: 1,
-                color: "rgba(213,72,120,0.3)",
+                color: "rgba(222, 70, 121, 0.514)",
               },
             ]),
           },
