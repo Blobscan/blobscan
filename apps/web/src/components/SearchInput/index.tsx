019c3cc767021e9241b312346ac6ec52d63dec35
PJColombo <paulo.colombo@pm.me>
2024-04-17 10:21:47 +0200

    fix(web): resolve search button oversize on some browsers

--- apps/web/src/components/SearchInput/index.tsx
+++ apps/web/src/components/SearchInput/index.tsx
@@ -137,7 +137,12 @@ export const SearchInput: React.FC<SearchInputProps> = function ({
           ring-1
           ring-inset
           `}
-          icon={<MagnifyingGlassIcon className="-ml-0.5" aria-hidden="true" />}
+          icon={
+            <MagnifyingGlassIcon
+              className="-ml-0.5 h-5 w-5"
+              aria-hidden="true"
+            />
+          }
           size="md"
         />
       </div>
