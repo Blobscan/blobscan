704b2a99176983264d060d5921e754b461ee4b04
wuyong <1524158692@qq.com>
2024-06-19 11:05:08 +0800

    feat:fix banner print dillscan

--- apps/rest-api-server/src/scripts/print-banner.ts
+++ apps/rest-api-server/src/scripts/print-banner.ts
@@ -3,12 +3,12 @@ import { env as apiEnv } from "@blobscan/api";
 import { env as restEnv } from "../env";
 
 function run() {
-  console.log(" ____  _   _   _    ");
-  console.log("|  _ \(_) | | | |___  ___ __ _ _ __    ");
-  console.log("| | | | | | | | / __|/ __/ _` | '_ \ ");
-  console.log("| |_| | | | | | \__ \ (_| (_| | | | |");
-  console.log("|____/|_| |_| |_|___/\___\__,_|_| |_|");
-  console.log("Dillscan REST API (EIP-4844 blob explorer) - dillscan.com");
+  console.log(" ____  _  _  _    ");
+  console.log("|  _ \\(_)| || |___  ___ __ _ _ __    ");
+  console.log("| | | | || || / __|/ __/ _` | '_ \\ ");
+  console.log("| |_| | || || \\__ \\ (__|(_| | | | |");
+  console.log("|____/|_||_||_|___/\\___\\__,_|_| |_|");
+  console.log("Dillscan REST API (EIP-4844 blob explorer) - dillscan.dill.xyz");
   console.log("====================================================\n");
 
   restEnv.display();
--- apps/web/banner.mjs
+++ apps/web/banner.mjs
@@ -3,12 +3,12 @@ function maskSensitiveData(sensitiveData) {
 }
 
 export function printBanner() {
-  console.log(" ____  _   _   _    ");
-  console.log("|  _ \(_) | | | |___  ___ __ _ _ __    ");
-  console.log("| | | | | | | | / __|/ __/ _` | '_ \ ");
-  console.log("| |_| | | | | | \__ \ (_| (_| | | | |");
-  console.log("|____/|_| |_| |_|___/\___\__,_|_| |_|");
-  console.log("Dillscan Web App (EIP-4844 blob explorer) - dillscan.com");
+  console.log(" ____  _  _  _    ");
+  console.log("|  _ \\(_)| || |___  ___ __ _ _ __    ");
+  console.log("| | | | || || / __|/ __/ _` | '_ \\ ");
+  console.log("| |_| | || || \\__ \\ (__|(_| | | | |");
+  console.log("|____/|_||_||_|___/\\___\\__,_|_| |_|");
+  console.log("Dillscan Web App (EIP-4844 blob explorer) - dillscan.dill.xyz");
   console.log("=======================================================\n");
   console.log(
     `Configuration: network=${process.env.NEXT_PUBLIC_NETWORK_NAME} explorer=${
