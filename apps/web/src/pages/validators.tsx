36f819f74fa7484b742307ca7563538abad679ed
wuyong <1524158692@qq.com>
2024-07-05 17:11:03 +0800

    feat: change public key color and state color.

--- apps/web/src/pages/validators.tsx
+++ apps/web/src/pages/validators.tsx
@@ -8,6 +8,7 @@ import NextError from "~/pages/_error";
 import { Card, CardContent, Grid, Typography } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import FileCopyIcon from '@mui/icons-material/FileCopy';
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import {convertWei } from "~/utils";
 
@@ -36,10 +37,10 @@ return (
         <CardContent>
         <Grid container spacing={3}>
             <Grid item xs={2}>
-              <Typography variant="h6" style={{ lineHeight: '60px' , display: 'flex', justifyContent: 'center', color: '#9370DB', fontWeight: 'bold'  }}>
+              <Typography variant="h6" style={{ lineHeight: '60px' , display: 'flex', justifyContent: 'center', color: '#143226', fontWeight: 'bold'  }}>
                 Public Key
               </Typography>
-              <div style={{ display: 'flex', justifyContent: 'center', color:"#87CEFA" }}>
+              <div style={{ display: 'flex', justifyContent: 'center', color:"#17a2b8" }}>
                 <Typography style={{ lineHeight: '40px' }} >
                   {validator.validator.pubkey.substring(0, 12)}...
                 </Typography>
@@ -51,27 +52,37 @@ return (
               </div>
             </Grid>
             <Grid item xs={1}>
-              <Typography variant="h6" style={{ lineHeight: '60px' , display: 'flex', justifyContent: 'center', color: '#9370DB', fontWeight: 'bold'  }}>Index</Typography>
-              <div style={{ display: 'flex', justifyContent: 'center', color:"#87CEFA" }}>
+              <Typography variant="h6" style={{ lineHeight: '60px' , display: 'flex', justifyContent: 'center', color: '#143226', fontWeight: 'bold'  }}>Index</Typography>
+              <div style={{ display: 'flex', justifyContent: 'center', color:"#17a2b8" }}>
                 <Typography style={{ lineHeight: '40px' }}>{validator.index}</Typography>
               </div>
             </Grid>
             <Grid item xs={2.5}>
-              <Typography variant="h6" style={{ lineHeight: '60px' , display: 'flex', justifyContent: 'center', color: '#9370DB', fontWeight: 'bold'  }}>Balance</Typography>
+              <Typography variant="h6" style={{ lineHeight: '60px' , display: 'flex', justifyContent: 'center', color: '#143226', fontWeight: 'bold'  }}>Balance</Typography>
               <Typography style={{ display: 'flex', justifyContent: 'center', lineHeight: '40px' }}>{ convertWei(validator.balance)} ETH ({convertWei(validator.validator.effective_balance)} ETH)</Typography>
             </Grid>
             <Grid item xs={1.5}>
-              <Typography variant="h6" style={{ lineHeight: '60px' , display: 'flex', justifyContent: 'center', color: '#9370DB', fontWeight: 'bold'  }}>State</Typography>
-              <Typography style={{ display: 'flex', justifyContent: 'center', lineHeight: '40px', color: validator.status.includes('active') ? '#00FA9A' : validator.status.includes('exited') ? 'red' : '' }}>
-                {validator.status}
-              </Typography>       
+              <Typography variant="h6" style={{ lineHeight: '60px' , display: 'flex', justifyContent: 'center', color: '#143226', fontWeight: 'bold'  }}>State</Typography>
+              <div style={{ display: 'flex', justifyContent: 'center'}}>
+                <Typography style={{lineHeight: '40px'}}>{validator.status}</Typography>
+                {
+                  validator.status.includes('active') ?
+                    <IconButton style={{color: '#32CD32'}}>
+                      <PowerSettingsNewIcon fontSize="small" />
+                    </IconButton>  : 
+                  validator.status.includes('exited') ?
+                    <IconButton style={{color: '#f82e2e'}}>
+                      <PowerSettingsNewIcon fontSize="small" />
+                    </IconButton> : null
+                 }
+            </div>   
             </Grid>
             <Grid item xs={1.5}>
-              <Typography variant="h6" style={{ lineHeight: '60px' , display: 'flex', justifyContent: 'center', color: '#9370DB', fontWeight: 'bold'  }}>Activation</Typography>
+              <Typography variant="h6" style={{ lineHeight: '60px' , display: 'flex', justifyContent: 'center', color: '#143226', fontWeight: 'bold'  }}>Activation</Typography>
               <Typography style={{ display: 'flex', justifyContent: 'center', lineHeight: '40px' }}>Epoch {validator.validator.activation_epoch}</Typography>       
             </Grid>
             <Grid item xs={1.5}>
-              <Typography variant="h6" style={{ lineHeight: '60px' , display: 'flex', justifyContent: 'center', color: '#9370DB', fontWeight: 'bold'  }}>Exit</Typography>
+              <Typography variant="h6" style={{ lineHeight: '60px' , display: 'flex', justifyContent: 'center', color: '#143226', fontWeight: 'bold'  }}>Exit</Typography>
                 {
                   validator.validator.exit_epoch === "18446744073709551615" ?
                   <Typography style={{ display: 'flex', justifyContent: 'center', lineHeight: '40px' }}>----</Typography> : 
@@ -79,7 +90,7 @@ return (
                 }     
             </Grid>
             <Grid item xs={1.5} >
-              <Typography variant="h6" style={{ lineHeight: '60px' , display: 'flex', justifyContent: 'center', color: '#9370DB', fontWeight: 'bold'  }}>W/able</Typography>
+              <Typography variant="h6" style={{ lineHeight: '60px' , display: 'flex', justifyContent: 'center', color: '#143226', fontWeight: 'bold'  }}>W/able</Typography>
               {
                 validator.validator.exit_epoch === "18446744073709551615" ?
                 <Typography style={{ display: 'flex', justifyContent: 'center', lineHeight: '40px' }}>----</Typography> :
@@ -95,7 +106,7 @@ return (
         <CardContent >
         <Grid container spacing={3}>
             <Grid item xs={2}>
-              <div style={{ display: 'flex', justifyContent: 'center', color:"#87CEFA" }}>
+              <div style={{ display: 'flex', justifyContent: 'center', color:"#17a2b8" }}>
                 <Typography style={{ lineHeight: '40px' }} >
                   {validator.validator.pubkey.substring(0, 12)}...
                 </Typography>
@@ -107,7 +118,7 @@ return (
               </div>
             </Grid>
             <Grid item xs={1}>
-              <div style={{ display: 'flex', justifyContent: 'center', color:"#87CEFA" }}>
+              <div style={{ display: 'flex', justifyContent: 'center', color:"#17a2b8" }}>
                 <Typography style={{ lineHeight: '40px' }}>{validator.index}</Typography>
               </div>
             </Grid>
@@ -115,9 +126,19 @@ return (
               <Typography style={{ display: 'flex', justifyContent: 'center', lineHeight: '40px' }}>{ convertWei(validator.balance)} ETH ({convertWei(validator.validator.effective_balance)} ETH)</Typography>
             </Grid>
             <Grid item xs={1.5}>
-            <Typography style={{ display: 'flex', justifyContent: 'center', lineHeight: '40px', color: validator.status.includes('active') ? '#00FA9A' : validator.status.includes('exited') ? 'red' : '' }}>
-                {validator.status}
-              </Typography>       
+            <div style={{ display: 'flex', justifyContent: 'center'}}>
+                <Typography style={{lineHeight: '40px'}}>{validator.status}</Typography>
+                {
+                  validator.status.includes('active') ?
+                    <IconButton style={{color: '#32CD32'}}>
+                      <PowerSettingsNewIcon fontSize="small" />
+                    </IconButton>  : 
+                  validator.status.includes('exited') ?
+                    <IconButton style={{color: '#f82e2e'}}>
+                      <PowerSettingsNewIcon fontSize="small" />
+                    </IconButton> : null
+                 }
+            </div>       
             </Grid>
             <Grid item xs={1.5}>
               <Typography style={{ display: 'flex', justifyContent: 'center', lineHeight: '40px' }}>Epoch {validator.validator.activation_epoch}</Typography>       
