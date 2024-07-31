680271b96f64d3be5b2a65b56552e9f352a9394d
Pablo Castellano <pablo@anche.no>
2024-07-31 12:25:04 +0200

    use for loop in postbuild.ts

--- apps/rest-api-server/scripts/postbuild.ts
+++ apps/rest-api-server/scripts/postbuild.ts
@@ -16,14 +16,17 @@ async function main() {
     "swagger-ui-dist"
   );
 
-  await $`cp ${path.join(swaggerUiDist, "swagger-ui.css")} dist`;
-  await $`cp ${path.join(swaggerUiDist, "swagger-ui-bundle.js")} dist`;
-  await $`cp ${path.join(
-    swaggerUiDist,
-    "swagger-ui-standalone-preset.js"
-  )} dist`;
-  await $`cp ${path.join(swaggerUiDist, "favicon-16x16.png")} dist`;
-  await $`cp ${path.join(swaggerUiDist, "favicon-32x32.png")} dist`;
+  const files = [
+    "swagger-ui.css",
+    "swagger-ui-bundle.js",
+    "swagger-ui-standalone-preset.js",
+    "favicon-16x16.png",
+    "favicon-32x32.png"
+  ];
+
+  for (const file of files) {
+    await $`cp ${path.join(swaggerUiDist, file)} dist`;
+  }
 
   console.log("postbuild: Swagger UI assets copied to `dist` directory");
 }
