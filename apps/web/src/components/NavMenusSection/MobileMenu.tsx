c19e2fb64f1ea7d5612c742bc1020377ede1ab0b
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-08-01 18:16:08 -0400

    feat: Update mobile menu component with the requested features

--- apps/web/src/components/NavMenusSection/MobileMenu.tsx
+++ apps/web/src/components/NavMenusSection/MobileMenu.tsx
@@ -1,65 +1,155 @@
-import { XMarkIcon } from "@heroicons/react/24/solid";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 
+import { BlobscanLogo } from "../BlobscanLogo";
 import { Button } from "../Button";
-import { ExpandibleMenuItem, MenuItem } from "./data";
+import { Collapsable } from "../Collapsable";
+import { Rotable } from "../Rotable";
+import { ThemeModeButton } from "../ThemeModeButton";
+import type { ExpandibleMenuItem, MenuItem } from "./data";
+import { MENU_DATA } from "./data";
 
-export function MobileMenu({
-  data,
-  closeMenu,
-  open,
-}: {
-  data: Array<MenuItem | ExpandibleMenuItem>;
-  closeMenu: () => void;
-  open: boolean;
-}) {
+export function MobileMenu() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [open]);
+
   return (
-    <div
-      className={`fixed left-0 top-0 z-10 h-full w-full overflow-y-auto bg-background-light p-4 shadow duration-300 dark:bg-background-dark ${
-        open ? "translate-x-0" : "-translate-x-full"
-      }`}
-    >
-      <div>
-        <Button variant="icon" onClick={closeMenu} icon={<XMarkIcon />} />
-        <div className="flex flex-col justify-center gap-4 p-2">
-          {data.map((item) => (
-            <MobileMenuItem {...item} key={`${item.label}-mobile`} />
-          ))}
+    <>
+      <Button
+        variant="icon"
+        className="md:hidden"
+        onClick={() => setOpen(true)}
+        icon={<Bars3Icon />}
+      />
+      <MobileMenuBackground open={open} close={() => setOpen(false)} />
+      <div
+        className={`fixed left-0 top-0 z-50 h-full w-[80%] overflow-y-auto border-r border-black border-opacity-20 bg-background-light p-4 duration-300 dark:bg-background-dark ${
+          open ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <BlobscanLogo className="mb-8 mt-4 w-40" />
+        <div className="flex flex-col justify-center gap-2 opacity-80">
+          {MENU_DATA.map((item, i) =>
+            item.type === "expandible" ? (
+              <ExpandibleItem {...item} key={`mobile-${i}`} />
+            ) : (
+              <SingleItem {...item} key={`mobile-${i}`} />
+            )
+          )}
+        </div>
+        <div className="absolute bottom-4 left-4">
+          <ThemeModeButton />
         </div>
       </div>
+    </>
+  );
+}
+
+function MobileMenuBackground({
+  open,
+  close,
+}: {
+  open: boolean;
+  close: () => void;
+}) {
+  useEffect(() => {
+    const keydownHandler = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        close();
+      }
+    };
+
+    document.addEventListener("keydown", keydownHandler);
+
+    return () => {
+      document.removeEventListener("keydown", keydownHandler);
+    };
+  }, [close]);
+
+  return (
+    <div
+      className={`fixed left-0 top-0 h-full w-full bg-black ${
+        open ? "opacity-80" : "pointer-events-none opacity-0"
+      }`}
+      onClick={close}
+      tabIndex={0}
+      role="button"
+      onKeyDown={(event) => {
+        if (event.key === "Escape") {
+          close();
+        }
+      }}
+    />
+  );
+}
+
+function SingleItem({ label, href, icon }: MenuItem) {
+  return (
+    <a
+      className="flex cursor-pointer items-center gap-1 p-2 duration-200 hover:text-iconHighlight-light hover:dark:text-iconHighlight-dark"
+      href={href}
+    >
+      <div className="h-5 w-5">{icon}</div>
+      {label}
+    </a>
+  );
+}
+
+function ExpandibleItem({ label, icon, items }: ExpandibleMenuItem) {
+  const pathname = usePathname();
+  const [open, setOpen] = useState(items.some((x) => x.href === pathname));
+
+  return (
+    <div>
+      <button
+        onClick={() => setOpen((value) => !value)}
+        className={`flex w-full items-center gap-3 rounded bg-opacity-20 p-2 ${
+          open
+            ? "border border-iconHighlight-dark border-opacity-20 bg-iconHighlight-dark bg-opacity-10 text-iconHighlight-light dark:text-iconHighlight-dark"
+            : ""
+        }`}
+      >
+        <Rotable angle={90} rotated={open}>
+          <ChevronDownIcon className="mb-2 h-5 w-5 -rotate-90" />
+        </Rotable>
+        <div className="flex items-center gap-1">
+          <div className="h-5 w-5">{icon}</div>
+          {label}
+        </div>
+      </button>
+      <Collapsable opened={open}>
+        <div className="flex flex-col gap-2 pb-4 pl-10 pt-2">
+          {items.map((subItem, index) => (
+            <ExpandibleSubItem {...subItem} key={`${label}-${index}-mobile`} />
+          ))}
+        </div>
+      </Collapsable>
     </div>
   );
 }
 
-function MobileMenuItem(item: MenuItem | ExpandibleMenuItem) {
-  if (item.type === "single") {
-    return (
-      <a
-        className="flex cursor-pointer items-center gap-1 duration-200 hover:text-iconHighlight-light hover:dark:text-iconHighlight-dark"
-        href={item.href}
-      >
-        <div className="h-4 w-4">{item.icon}</div>
-        {item.label}
-      </a>
-    );
-  }
+function ExpandibleSubItem({ label, href }: { label: string; href: string }) {
+  const pathname = usePathname();
 
   return (
-    <div>
-      <div className="flex items-center gap-1">
-        <div className="h-4 w-4">{item.icon}</div>
-        {item.label}
-      </div>
-      <div className="flex flex-col gap-2 p-4">
-        {item.items.map((subItem, index) => (
-          <a
-            key={`${subItem.label}-${index}-mobile`}
-            href={subItem.href}
-            className="duration-200 hover:text-iconHighlight-light hover:dark:text-iconHighlight-dark"
-          >
-            {subItem.label}
-          </a>
-        ))}
-      </div>
-    </div>
+    <a
+      href={href}
+      className={`text-sm duration-200 ${
+        href === pathname
+          ? "text-iconHighlight-light dark:text-iconHighlight-dark"
+          : "hover:text-iconHighlight-light hover:dark:text-iconHighlight-dark"
+      }`}
+    >
+      {label}
+    </a>
   );
 }
--- apps/web/src/components/NavMenusSection/index.tsx
+++ apps/web/src/components/NavMenusSection/index.tsx
@@ -1,42 +1,17 @@
-import { useEffect, useState } from "react";
-import { Bars3Icon } from "@heroicons/react/24/solid";
-
-import { Button } from "../Button";
 import { MobileMenu } from "./MobileMenu";
 import { NavItem } from "./NavItem";
 import { MENU_DATA } from "./data";
 
 export const NavMenusSection: React.FC = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-  useEffect(() => {
-    if (mobileMenuOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
-    }
-  }, [mobileMenuOpen]);
-
   return (
     <div className="flex items-center justify-between">
-      <Button
-        variant="icon"
-        className="md:hidden"
-        onClick={() => setMobileMenuOpen(true)}
-        icon={<Bars3Icon />}
-      />
+      <MobileMenu />
 
       <div className="hidden items-center gap-4 md:flex">
         {MENU_DATA.map((item) => (
           <NavItem {...item} key={item.label} />
         ))}
       </div>
-
-      <MobileMenu
-        data={MENU_DATA}
-        open={mobileMenuOpen}
-        closeMenu={() => setMobileMenuOpen(false)}
-      />
     </div>
   );
 };
