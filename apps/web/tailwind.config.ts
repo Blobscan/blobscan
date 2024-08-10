5c96176e026c7e3c3545d1e12937e5d3790d5227
xfja <franjimenezaguilera@gmail.com>
2024-08-10 12:46:50 +0200

    feat(web): install React DatePicker Tailwind library

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -3,6 +3,9 @@ import type { Config } from "tailwindcss";
 import baseConfig from "@blobscan/tailwind-config";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./src/**/*.tsx",
+    "../../node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+  ],
   presets: [baseConfig],
 } satisfies Config;
