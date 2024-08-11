72fccf88654ddbb6dc6869d57bb5e920d9e5ef20
PJColombo <paulo.colombo@pm.me>
2024-08-11 12:14:31 +0200

    style(web): rename animated side panel props

--- apps/web/src/components/SidePanel.tsx
+++ apps/web/src/components/SidePanel.tsx
@@ -53,9 +53,7 @@ export function SidePanel({
 
       <animated.div
         className={`fixed top-0 z-50 h-full w-[80%] max-w-[400px] overflow-y-auto border-r border-black border-opacity-20 bg-background-light dark:bg-background-dark`}
-        style={{
-          left: props.left,
-        }}
+        style={props}
       >
         <SidePanelContext.Provider value={{ status }}>
           {children}
@@ -77,8 +75,8 @@ export function useSidePanel() {
 
 function Overlay({ show, onClose }: { show: boolean; onClose: () => void }) {
   const props = useSpring({
-    from: { showProgress: 0 },
-    to: { showProgress: Number(show) },
+    from: { opacity: 0 },
+    to: { opacity: show ? 0.8 : 0 },
   });
 
   return (
@@ -86,9 +84,7 @@ function Overlay({ show, onClose }: { show: boolean; onClose: () => void }) {
       className={cn("fixed left-0 top-0 z-10 h-full w-full bg-black", {
         "pointer-events-none": !show,
       })}
-      style={{
-        opacity: props.showProgress.to((value) => value * 0.8),
-      }}
+      style={props}
       onClick={onClose}
       tabIndex={0}
       role="button"
