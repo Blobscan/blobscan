9667695c96240b47a3996869e62719ff1ebea9e9
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-08-27 00:30:33 -0400

    refactor: Update Tooltip component to use @floating-ui/react library

--- apps/web/src/components/Tooltip.tsx
+++ apps/web/src/components/Tooltip.tsx
@@ -1,81 +1,173 @@
+import {
+  cloneElement,
+  createContext,
+  forwardRef,
+  isValidElement,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import type { ReactNode } from "react";
-import { useEffect, useRef, useState } from "react";
+import {
+  useFloating,
+  autoUpdate,
+  offset,
+  flip,
+  shift,
+  useHover,
+  useFocus,
+  useDismiss,
+  useRole,
+  useInteractions,
+  FloatingPortal,
+  FloatingArrow,
+  arrow,
+  useMergeRefs,
+} from "@floating-ui/react";
 
-type TooltipProps = {
-  show: boolean;
+type TooltipOptions = {
   children: ReactNode;
+  onChange?: (isOpen: boolean) => void;
 };
 
-type Position = "top" | "right" | "bottom" | "left";
+function useTooltip({ onChange }: TooltipOptions) {
+  const [isOpen, setIsOpen] = useState(false);
 
-function getOverflow(element: HTMLDivElement) {
-  const rect = element.getBoundingClientRect();
+  const arrowRef = useRef(null);
 
-  return {
-    top: rect.top < 0,
-    right: rect.right > window.innerWidth,
-    bottom: rect.bottom > window.innerHeight,
-    left: rect.left < 0,
-  };
-}
+  const data = useFloating({
+    placement: "top",
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    whileElementsMounted: autoUpdate,
+    middleware: [
+      shift(),
+      offset(10),
+      flip({ fallbackAxisSideDirection: "start" }),
+      arrow({ element: arrowRef }),
+    ],
+  });
 
-export function Tooltip({ show, children }: TooltipProps) {
-  const divRef = useRef<HTMLDivElement>(null);
-  const [position, setPosition] = useState<Position>("top");
+  const context = data.context;
+
+  const hover = useHover(context, { move: false });
+  const focus = useFocus(context);
+  const dismiss = useDismiss(context);
+  const role = useRole(context, { role: "tooltip" });
+
+  const interactions = useInteractions([hover, focus, dismiss, role]);
 
   useEffect(() => {
-    if (!divRef.current) {
-      return;
+    if (onChange) {
+      onChange(isOpen);
     }
+  }, [isOpen, onChange]);
 
-    const overflow = getOverflow(divRef.current);
-
-    if (overflow.top) {
-      setPosition("bottom");
-    } else if (overflow.right) {
-      setPosition("left");
-    } else if (overflow.left) {
-      setPosition("right");
-    } else if (overflow.bottom) {
-      setPosition("top");
-    } else {
-      setPosition("top");
-    }
-  }, [show]);
-
-  return (
-    <div
-      ref={divRef}
-      className={`pointer-events-none absolute z-10 font-normal ${
-        show ? "opacity-100" : "opacity-0"
-      } ${
-        position === "top"
-          ? "-top-2 left-[50%] -translate-x-[50%] translate-y-[-100%]"
-          : ""
-      } ${
-        position === "bottom"
-          ? "-bottom-2 left-[50%] -translate-x-[50%] translate-y-[100%]"
-          : ""
-      } ${
-        position === "left"
-          ? "-left-2 top-[50%] -translate-x-[100%] translate-y-[-50%]"
-          : ""
-      } ${
-        position === "right"
-          ? "-right-2 top-[50%] translate-x-[100%] translate-y-[-50%]"
-          : ""
-      }`}
-    >
-      <div className="rounded-lg bg-accent-light p-3 text-xs text-white dark:bg-primary-500">
-        {children}
-      </div>
-      <div
-        className={`absolute h-2 w-2 -translate-x-[50%] translate-y-[50%] rotate-45 bg-accent-light dark:bg-primary-500 ${
-          position === "top" ? "bottom-[0%] left-[50%]" : ""
-        } ${position === "bottom" ? "bottom-[100%] left-[50%]" : ""} ${
-          position === "left" ? "bottom-[50%] left-[100%]" : ""
-        } ${position === "right" ? "bottom-[50%] left-[0%]" : ""}`}
-      />
-    </div>
+  return useMemo(
+    () => ({
+      isOpen,
+      setIsOpen,
+      ...interactions,
+      ...data,
+      arrowRef,
+    }),
+    [isOpen, setIsOpen, interactions, data, arrowRef]
   );
 }
+
+type ContextType = ReturnType<typeof useTooltip> | null;
+
+const TooltipContext = createContext<ContextType>(null);
+
+export const useTooltipContext = () => {
+  const context = useContext(TooltipContext);
+
+  if (context == null) {
+    throw new Error("Tooltip components must be wrapped in <Tooltip />");
+  }
+
+  return context;
+};
+
+export function Tooltip(options: TooltipOptions) {
+  const tooltip = useTooltip(options);
+
+  return (
+    <TooltipContext.Provider value={tooltip}>
+      {options.children}
+    </TooltipContext.Provider>
+  );
+}
+
+export const TooltipTrigger = forwardRef<
+  HTMLElement,
+  React.HTMLProps<HTMLElement> & { asChild?: boolean }
+>(function TooltipTrigger({ children, asChild = false, ...props }, propRef) {
+  const context = useTooltipContext();
+
+  const childrenRef =
+    isValidElement(children) && "ref" in children
+      ? (children.ref as React.Ref<HTMLElement>)
+      : null;
+
+  const ref = useMergeRefs([context.refs.setReference, propRef, childrenRef]);
+
+  if (asChild && isValidElement(children)) {
+    return cloneElement(
+      children,
+      context.getReferenceProps({
+        ref,
+        ...props,
+        ...children.props,
+        "data-state": context.isOpen ? "open" : "closed",
+      })
+    );
+  }
+
+  return (
+    <button
+      ref={ref}
+      data-state={context.isOpen ? "open" : "closed"}
+      {...context.getReferenceProps(props)}
+    >
+      {children}
+    </button>
+  );
+});
+
+export const TooltipContent = forwardRef<
+  HTMLDivElement,
+  React.HTMLProps<HTMLDivElement>
+>(function TooltipContent({ style, children, ...props }, propRef) {
+  const context = useTooltipContext();
+  const ref = useMergeRefs([context.refs.setFloating, propRef]);
+
+  if (!context.isOpen) {
+    return null;
+  }
+
+  return (
+    <FloatingPortal>
+      <div
+        ref={ref}
+        style={{
+          ...context.floatingStyles,
+          ...style,
+        }}
+        {...context.getFloatingProps(props)}
+      >
+        <div className="p- bg-accent-light3 rounded-lg text-xs font-normal text-white dark:bg-primary-500">
+          {children}
+        </div>
+
+        <FloatingArrow
+          ref={context.arrowRef}
+          context={context.context}
+          className="fill-accent-light dark:fill-primary-500"
+        />
+      </div>
+    </FloatingPortal>
+  );
+});
