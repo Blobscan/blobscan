1a59e09293350292b0a4006c1fdd0a9f74bf3683
PJColombo <paulo.colombo@pm.me>
2024-08-28 17:10:57 +0200

    test(rollups): add tests to verify correct behavior when retrieving non-existent values

--- packages/rollups/test/rollups.test.ts
+++ packages/rollups/test/rollups.test.ts
@@ -9,7 +9,7 @@ import {
 } from "../src";
 
 describe("Rollups", () => {
-  it("should get a rollup by its address correctly", () => {
+  it("should retrieve a rollup by its address correctly", () => {
     const chainId = 1;
     const address = "0x5050f69a9786f081509234f1a7f4684b5e5b76c9";
     const expectedRollup = Rollup.BASE;
@@ -17,12 +17,26 @@ describe("Rollups", () => {
     expect(getRollupByAddress(address, chainId)).toBe(expectedRollup);
   });
 
-  it("should get an address by its rollup correctly", () => {
+  it("should return null when retrieving a rollup that does not exists", () => {
+    const chainId = 1;
+    const address = "0x12321312321321321321";
+
+    expect(getRollupByAddress(address, chainId)).toBe(null);
+  });
+
+  it("should retrieve an address by its rollup correctly", () => {
     const chainId = 11155111;
     const rollup = Rollup.MODE;
     const expectedAddress = "0x4e6bd53883107b063c502ddd49f9600dc51b3ddc";
 
     expect(getAddressByRollup(rollup, chainId)).toBe(expectedAddress);
+  });
+
+  it("should return null when retrieving an address by a rollup that does not exists", () => {
+    const chainId = 11155111;
+    const rollup = Rollup.BOBA;
+
+    expect(getAddressByRollup(rollup, chainId)).toBe(null);
   });
 
   it("should get all chain's rollups correctly", () => {
@@ -40,5 +54,11 @@ describe("Rollups", () => {
     ];
 
     expect(getChainRollups(chainId)).toEqual(expectedRollup);
+  });
+
+  it("should return an empty array when when retrieving the rollups of a chain that does not exists", () => {
+    const chainId = 12323;
+
+    expect(getChainRollups(chainId)).toEqual([]);
   });
 });
