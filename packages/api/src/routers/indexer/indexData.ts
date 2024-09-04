2642d63007d34e89fd4048da37cfd9f37e809ce2
Pablo Castellano <pablo@anche.no>
2024-09-04 16:37:21 +0200

    feat: added download.ts script + refactor

--- packages/api/src/routers/indexer/indexData.ts
+++ packages/api/src/routers/indexer/indexData.ts
@@ -120,6 +120,13 @@ export const indexData = jwtAuthedProcedure
       const dbBlobs = createDBBlobs(input);
       const dbBlobsOnTransactions = createDBBlobsOnTransactions(input);
 
+      /*
+      TODO: Once we support Optimism Fjord network upgrade
+      if (rollup optimism in any tx in dbTxs) {
+        launch background task to decode blobs
+      }
+      */
+
       operations.push(
         prisma.block.upsert({
           where: { hash: input.block.hash },
--- packages/optimism-decoder/src/index.ts
+++ packages/optimism-decoder/src/index.ts
@@ -1,4 +1,31 @@
-import { processFile } from "./utils";
+import { decodeOptimismFile } from "./decoder";
 
-const filename = "opstack_blobs_19538908.bin";
-processFile(filename);
+export { decodeOptimismFile } from "./decoder";
+
+const args = process.argv.slice(2); // Slice to get arguments after 'node' and script path
+const filename = args[0]; // The first argument should be the filename
+// const filename = "opstack_blobs_19538908.bin";
+
+if (!filename) {
+  console.error("Error: Please provide a filename as a parameter.");
+  process.exit(1);
+}
+
+decodeOptimismFile(filename).then((data) => {
+  console.log("Timestamp since L2 genesis:", data.timestampSinceL2Genesis);
+  console.log("Last L1 origin number:", data.lastL1OriginNumber);
+  console.log("Parent L2 block hash:", data.parentL2BlockHash);
+  console.log("L1 origin block hash:", data.l1OriginBlockHash);
+  console.log("Number of L2 blocks:", data.numberOfL2Blocks);
+  console.log("How many were changed by L1 origin:", data.changedByL1Origin);
+  console.log("Total txs:", data.totalTxs);
+  console.log("Contract creation txs number:", data.contractCreationTxsNumber);
+  /*
+    console.log("Legacy txs number:", data.legacyTxsNumber);
+    console.log("Total gas limit in txs:", data.totalGasLimit);
+    console.log(
+      "Number of EIP-155 protected legacy txs:",
+      data.protectedLegacyTxsNumber
+    );
+    */
+});
