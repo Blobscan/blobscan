af00b62076c6c34ec7df56662b6dfee42ab09a96
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-09-04 12:36:21 -0400

    refactor: Update NavArrows component to use IconButton

--- apps/web/src/components/NavArrows.tsx
+++ apps/web/src/components/NavArrows.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 
+import { IconButton } from "./IconButton";
+
 export function NavArrows({ next, prev }: { next?: string; prev?: string }) {
   return (
     <div className="flex items-center justify-center gap-1">
@@ -14,30 +16,31 @@ function NavArrow({ type, href }: { type: "next" | "prev"; href?: string }) {
   const router = useRouter();
 
   return (
-    <button
+    <IconButton
       onClick={() => {
         if (href) {
           router.push(href);
         }
       }}
       className={`
-            dark:bg-neutral-850 
-            bg-border-border-dark
-            rounded-md 
-            border 
-            border-border-light
-            bg-white
-            p-1
-            dark:border-border-dark
-            dark:bg-border-dark
-            ${href ? "" : "cursor-default opacity-50"}
-        `}
+          dark:bg-neutral-850
+          bg-border-border-dark
+          rounded-md
+          border
+          border-border-light
+          bg-white
+          p-1
+          dark:border-border-dark
+          dark:bg-border-dark
+          ${href ? "" : "cursor-default opacity-50"}
+      `}
+      disabled={!href}
     >
       {type === "next" ? (
         <ChevronRightIcon className="h-4 w-4" />
       ) : (
         <ChevronLeftIcon className="h-4 w-4" />
       )}
-    </button>
+    </IconButton>
   );
 }
