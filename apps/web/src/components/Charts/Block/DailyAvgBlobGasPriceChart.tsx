fa09518c6a89b88d7ccc70e145df3d77ede08668
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-09-18 19:26:30 -0400

    refactor: Update chart tooltips to use best unit

--- apps/web/src/components/Charts/Block/DailyAvgBlobGasPriceChart.tsx
+++ apps/web/src/components/Charts/Block/DailyAvgBlobGasPriceChart.tsx
@@ -1,10 +1,12 @@
 import type { FC } from "react";
 import type { EChartOption } from "echarts";
 
+import { findBestUnit, formatWei, prettyFormatWei } from "@blobscan/eth-units";
+
 import { ChartCard } from "~/components/Cards/ChartCard";
 import { useScaledWeiAmounts } from "~/hooks/useScaledWeiAmounts";
 import type { DailyBlockStats } from "~/types";
-import { buildTimeSeriesOptions, formatNumber } from "~/utils";
+import { buildTimeSeriesOptions } from "~/utils";
 
 export type DailyAvgBlobGasPriceChartProps = {
   days: DailyBlockStats["days"];
@@ -14,14 +16,14 @@ export type DailyAvgBlobGasPriceChartProps = {
 export const DailyAvgBlobGasPriceChart: FC<
   Partial<DailyAvgBlobGasPriceChartProps>
 > = function ({ days, avgBlobGasPrices }) {
-  const { scaledValues, unit } = useScaledWeiAmounts(avgBlobGasPrices);
+  const { unit } = useScaledWeiAmounts(avgBlobGasPrices);
 
   const options: EChartOption<EChartOption.Series> = {
     ...buildTimeSeriesOptions({
       dates: days,
       axisFormatters: {
-        yAxisTooltip: (value) => `${formatNumber(value, "compact")} ${unit}`,
-        yAxisLabel: (value) => `${formatNumber(value, "compact")} ${unit}`,
+        yAxisTooltip: (value) => formatWei(value, findBestUnit(value)),
+        yAxisLabel: (value) => prettyFormatWei(value, unit),
       },
       yUnit: "ethereum",
     }),
@@ -31,7 +33,7 @@ export const DailyAvgBlobGasPriceChart: FC<
     series: [
       {
         name: "Avg. Blob Gas Prices",
-        data: scaledValues,
+        data: avgBlobGasPrices,
         type: "line",
       },
     ],
