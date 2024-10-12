5241142d07b9cb852fe0008e76c32f775d236cbc
Fran Jimenez Aguilera <franjimenezaguilera@gmail.com>
2024-10-12 15:48:18 +0200

    feat(web): add multi select prop to RollupFilter

--- apps/web/src/components/Filters/RollupFilter.tsx
+++ apps/web/src/components/Filters/RollupFilter.tsx
@@ -3,14 +3,16 @@ import type { FC } from "react";
 import { getChainRollups } from "@blobscan/rollups";
 
 import { Dropdown } from "~/components/Dropdown";
-import type { DropdownProps, Option } from "~/components/Dropdown";
+import type { Option } from "~/components/Dropdown";
 import { RollupIcon } from "~/components/RollupIcon";
 import { env } from "~/env.mjs";
 import type { Rollup } from "~/types";
 import { capitalize, getChainIdByName } from "~/utils";
+import { RollupBadge } from "../Badges/RollupBadge";
 
-type RollupFilterProps = Pick<DropdownProps, "selected"> & {
-  onChange(newRollup: Option | null): void;
+type RollupFilterProps = {
+  onChange(newRollup: Option[]): void;
+  selected: Option[] | null;
 };
 
 const chainId = getChainIdByName(env.NEXT_PUBLIC_NETWORK_NAME);
@@ -21,15 +23,16 @@ export const ROLLUP_OPTIONS: Option[] = [
     value: "null",
     label: "None",
   },
-  ...rollups.map(([rollupAddress, rollupName]) => ({
-    value: rollupAddress,
-    label: (
-      <div className="flex items-center gap-2">
-        <RollupIcon rollup={rollupName.toLowerCase() as Rollup} />
-        {capitalize(rollupName)}
-      </div>
-    ),
-  })),
+  ...rollups.map(([rollupAddress, rollupName]) => {
+    return {
+      value: rollupAddress,
+      inputDisplay: (
+        <RollupBadge rollup={rollupName.toLowerCase() as Rollup} size="sm" />
+      ),
+      prefix: <RollupIcon rollup={rollupName.toLowerCase() as Rollup} />,
+      label: capitalize(rollupName),
+    };
+  }),
 ];
 
 export const RollupFilter: FC<RollupFilterProps> = function ({
@@ -41,8 +44,9 @@ export const RollupFilter: FC<RollupFilterProps> = function ({
       selected={selected}
       options={ROLLUP_OPTIONS}
       onChange={onChange}
+      multiple={true}
       placeholder="Rollup"
-      width="sm:w-[130px] w-full md:max-lg:w-full"
+      width="sm:w-[130px] w-full xl:w-[240px] md:max-lg:w-full"
       clearable
     />
   );
