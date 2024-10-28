ee145aa13247a51a2fd996735d83fbb6e0c5a631
PJColombo <paulo.colombo@pm.me>
2024-10-28 01:53:55 +0100

    refactor(web): encapsulate blobscan external social links to a component

--- apps/web/src/components/AppLayout/BottomBarLayout/index.tsx
+++ apps/web/src/components/AppLayout/BottomBarLayout/index.tsx
@@ -1,40 +1,15 @@
-import type { ReactElement } from "react";
 import React from "react";
 
+import { BlobscanSocialLinks } from "~/components/BlobscanSocialLinks";
 import { BlobscanVersionInfo } from "~/components/BlobscanVersionInfo";
 import { ExplorerDetails } from "~/components/ExplorerDetails";
-import { IconButton } from "~/components/IconButton";
 import { Link } from "~/components/Link";
-import DiscordIcon from "~/icons/discord.svg";
-import GithubIcon from "~/icons/github.svg";
-import XIcon from "~/icons/x.svg";
-
-const EXTERNAL_APPS: { href: string; icon: ReactElement }[] = [
-  {
-    icon: <GithubIcon className="h-5 w-5" />,
-    href: "https://github.com/Blobscan/blobscan",
-  },
-  {
-    icon: <DiscordIcon className="h-5 w-5" />,
-    href: "https://discord.gg/6KNZ2UVFRt",
-  },
-  {
-    icon: <XIcon className="h-5 w-5" />,
-    href: "https://twitter.com/blobscan",
-  },
-];
 
 export const BottomBarLayout = () => {
   return (
     <div className="flex flex-col items-center justify-center p-2">
       <div className="mt-4 flex flex-col items-center gap-2 sm:mt-8">
-        <div className="flex items-center gap-2">
-          {EXTERNAL_APPS.map(({ icon, href }) => (
-            <Link key={href} href={href} isExternal hideExternalIcon>
-              <IconButton>{icon}</IconButton>
-            </Link>
-          ))}
-        </div>
+        <BlobscanSocialLinks />
         <div className="max-w-lg text-center text-xs text-contentTertiary-light dark:text-contentTertiary-dark">
           Blobscan is the first open-source block explorer for the{" "}
           <Link href="https://www.eip4844.com/" isExternal>
