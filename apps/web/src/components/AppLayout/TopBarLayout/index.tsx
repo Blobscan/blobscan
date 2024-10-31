e6a4850c0b0429078647efdb12d97014c61b3211
PJColombo <paulo.colombo@pm.me>
2024-10-31 14:10:11 +0100

    feat(web): add gitcoin banner

--- apps/web/src/components/AppLayout/TopBarLayout/index.tsx
+++ apps/web/src/components/AppLayout/TopBarLayout/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
+import GitcoinBanner from "~/components/GitcoinBanner";
 import { SidebarNavigationMenu } from "~/components/SidebarNavigationMenu";
 import { ThemeModeButton } from "~/components/ThemeModeButton";
 import { useIsHomepage } from "~/hooks/useIsHomePage";
@@ -10,25 +11,29 @@ import { TopBar } from "./TopBar";
 
 export const TopBarLayout: React.FC = () => {
   const isHomepage = useIsHomepage();
+  const [showBanner, setShowBanner] = useState(true);
 
   if (isHomepage) {
     return (
-      <nav className="z-10 flex h-16 w-full items-center justify-end px-4 md:justify-between">
-        <div className="ml-5 w-full md:ml-0 md:flex">
-          <ExplorerDetails placement="top" />
-        </div>
-        <div className="flex items-center gap-3">
-          <div className="xl:hidden">
-            <SidebarNavigationMenu />
+      <>
+        {showBanner && <GitcoinBanner onClose={() => setShowBanner(false)} />}
+        <nav className="z-10 flex h-16 w-full items-center justify-end px-4 md:justify-between">
+          <div className="ml-5 w-full md:ml-0 md:flex">
+            <ExplorerDetails placement="top" />
           </div>
-          <div className="hidden xl:flex">
-            <NavigationMenus />
+          <div className="flex items-center gap-3">
+            <div className="xl:hidden">
+              <SidebarNavigationMenu />
+            </div>
+            <div className="hidden xl:flex">
+              <NavigationMenus />
+            </div>
+            <div className="relative ml-2 hidden xl:block">
+              <ThemeModeButton />
+            </div>
           </div>
-          <div className="relative ml-2 hidden xl:block">
-            <ThemeModeButton />
-          </div>
-        </div>
-      </nav>
+        </nav>
+      </>
     );
   }
 
@@ -37,6 +42,7 @@ export const TopBarLayout: React.FC = () => {
       <div
         className={`z-10 hidden h-16 w-full items-center justify-between sm:block`}
       >
+        {showBanner && <GitcoinBanner onClose={() => setShowBanner(false)} />}
         <TopBar />
       </div>
       <CompactTopBar />
