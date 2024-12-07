c8c5b4b81241af99b77f51dea13deb5bb0e3361c
PJColombo <paulo.colombo@pm.me>
2024-12-07 07:57:16 +0100

    refactor(stats-aggregation-cli): aggregate daily stats by batches

--- clis/stats-aggregation-cli/src/utils.ts
+++ clis/stats-aggregation-cli/src/utils.ts
@@ -1,5 +1,6 @@
 import type commandLineUsage from "command-line-usage";
 
+import { toDailyDate } from "@blobscan/dayjs";
 import { logger } from "@blobscan/logger";
 
 import type { Operation } from "./types";
@@ -16,6 +17,34 @@ export const deleteOptionDef: commandLineUsage.OptionDefinition = {
   alias: "d",
   description: "Delete existing stats.",
   type: Boolean,
+};
+
+const dateType = (input: string) => {
+  const date = toDailyDate(input);
+
+  if (!date.isValid()) {
+    throw new Error(`Invalid date "${input}". Expected a ISO 8601 date.`);
+  }
+
+  return date.toISOString();
+};
+
+export const datePeriodOptionDefs: Record<
+  "to" | "from",
+  commandLineUsage.OptionDefinition
+> = {
+  from: {
+    name: "from",
+    typeLabel: "{underline from-date}",
+    description: "Date from which execute jobs.",
+    type: dateType,
+  },
+  to: {
+    name: "to",
+    typeLabel: "{underline to-date}",
+    description: "Date to which execute jobs.",
+    type: dateType,
+  },
 };
 
 export function capitalize(s: string) {
