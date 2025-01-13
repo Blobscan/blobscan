322d1a871e09dde478b49c09f6b10138cf8bc5f5
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2025-01-12 21:43:32 -0400

    fix: rename variable current_url to currentUrl for consistency

--- packages/api/src/context.ts
+++ packages/api/src/context.ts
@@ -118,14 +118,14 @@ export function createTRPCContext(
           );
         }
 
-        let current_url = opts.req.url;
+        let currentUrl = opts.req.url;
         let pathname: string | undefined;
         let query: string | undefined;
 
         if (opts.req.url) {
           const url = new URL(opts.req.url, env.BLOBSCAN_API_BASE_URL);
           pathname = url.pathname;
-          current_url = url.toString();
+          currentUrl = url.toString();
           query = url.searchParams.toString();
         }
 
@@ -135,7 +135,7 @@ export function createTRPCContext(
           properties: {
             $ip: getIP(opts.req),
             scope,
-            $current_url: current_url,
+            $current_url: currentUrl,
             network: env.NETWORK_NAME,
             pathname,
             query,
