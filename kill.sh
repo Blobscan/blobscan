6b886383e4856859b5dc70e0b808728b5592b93b
wuyong <1524158692@qq.com>
2025-02-04 22:19:28 +0800

    feat: add withdrawal_amount to validator_income

--- kill.sh
+++ kill.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ps aux | grep -i scan | grep -v grep | awk '{print $2}' | xargs kill -9
+ps aux | grep -i scan | grep -v grep | grep -v indexer | grep -v rewards | awk '{print $2}' | xargs kill -9
 
 ps aux | grep -i next-router-wo | grep -v grep | awk '{print $2}' | xargs kill -9
