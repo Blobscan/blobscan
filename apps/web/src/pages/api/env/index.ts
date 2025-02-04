58d9788c5516d37aa3b0a7e6fde7bf04e5ff5094
Fran Jimenez Aguilera <franjimenezaguilera@gmail.com>
2025-02-04 20:03:58 +0700

    feat(web): update client vars schema

--- apps/web/src/pages/api/env/index.ts
+++ apps/web/src/pages/api/env/index.ts
@@ -1,27 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { env } from "@blobscan/env";
-
-import { clientEnvVarsSchema } from "../../../env.mjs";
+import { env } from "../../../env.mjs";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const method = req.method;
 
   switch (method) {
     case "GET": {
-      const parsedEnv = clientEnvVarsSchema.safeParse(env);
-
-      if (!parsedEnv.success) {
-        return res.status(400).json({
-          error: "Error parsing client side environment variables",
-          details: parsedEnv.error.format(),
-        });
-      }
-
       const clientEnv = Object.fromEntries(
-        Object.entries(parsedEnv.data)
-          .filter(([key]) => key.startsWith("NEXT_"))
-          .map(([key, value]) => [key.replace(/^NEXT_/, ""), value])
+        Object.entries(env).filter(([key]) => key.startsWith("PUBLIC"))
       );
 
       return res.status(200).json(clientEnv);
