3d65e837588075a3cd5c214a47629704e1690568
kilavvy <140459108+kilavvy@users.noreply.github.com>
2025-02-19 18:19:46 +0100

    fix: typos in documentation files (#727)
    * chore(api): Removes PostHog configuration and related code (#707)
    
    * chore(api): Removes PostHog configuration and related code
    
    * chore(api): Added changeset
    
    * Delete .changeset/red-beers-count.md
    
    * Update helpers.ts
    
    * Update blob.test.ts.snap
    
    * Update tx.test.ts.snap
    
    ---------
    
    Co-authored-by: Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>

--- packages/api/src/context.ts
+++ packages/api/src/context.ts
@@ -6,20 +6,15 @@ import type {
   NodeHTTPRequest,
   NodeHTTPResponse,
 } from "@trpc/server/adapters/node-http";
-import cookie from "cookie";
 
 import type { BlobPropagator } from "@blobscan/blob-propagator";
 import { getBlobPropagator } from "@blobscan/blob-propagator";
 import type { BlobStorageManager } from "@blobscan/blob-storage-manager";
 import { getBlobStorageManager } from "@blobscan/blob-storage-manager";
 import { prisma } from "@blobscan/db";
-import { env } from "@blobscan/env";
 
-import { PostHogClient, shouldIgnoreURL } from "./posthog";
 import type { APIClient } from "./utils";
 import { retrieveAPIClient } from "./utils";
-
-type NextHTTPRequest = CreateNextContextOptions["req"];
 
 export type CreateContextOptions =
   | NodeHTTPCreateContextFnOptions<NodeHTTPRequest, NodeHTTPResponse>
@@ -50,16 +45,6 @@ export async function createTRPCInnerContext(
   };
 }
 
-function getIP(req: NodeHTTPRequest | NextHTTPRequest): string | undefined {
-  const ip = req.headers["x-forwarded-for"] ?? req.socket.remoteAddress;
-
-  if (Array.isArray(ip)) {
-    return ip[0];
-  }
-
-  return ip;
-}
-
 type ContextScope = "web" | "rest-api";
 
 export function createTRPCContext({ scope }: { scope: ContextScope }) {
@@ -70,53 +55,6 @@ export function createTRPCContext({ scope }: { scope: ContextScope }) {
       const innerContext = await createTRPCInnerContext({
         apiClient,
       });
-
-      const posthog = PostHogClient();
-
-      if (posthog && !shouldIgnoreURL(opts.req.url)) {
-        const cookies = cookie.parse(opts.req.headers.cookie ?? "");
-
-        let distinctId = cookies["distinctId"];
-
-        if (!distinctId) {
-          distinctId = crypto.randomUUID();
-
-          opts.res.setHeader(
-            "Set-Cookie",
-            cookie.serialize("distinctId", distinctId, {
-              maxAge: 60 * 60 * 24 * 365,
-              httpOnly: false,
-              path: "/",
-            })
-          );
-        }
-
-        let currentUrl = opts.req.url;
-        let pathname: string | undefined;
-        let query: string | undefined;
-
-        if (opts.req.url) {
-          const url = new URL(opts.req.url, env.BLOBSCAN_API_BASE_URL);
-          pathname = url.pathname;
-          currentUrl = url.toString();
-          query = url.searchParams.toString();
-        }
-
-        posthog.capture({
-          distinctId,
-          event: "trpc_request",
-          properties: {
-            $ip: getIP(opts.req),
-            scope,
-            $current_url: currentUrl,
-            network: env.NETWORK_NAME,
-            pathname,
-            query,
-          },
-        });
-
-        await posthog.shutdown();
-      }
 
       return {
         ...innerContext,
