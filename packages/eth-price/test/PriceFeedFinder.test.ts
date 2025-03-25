36f20d79b6b56ead806f3cae2f09ad06e4968de7
PJColombo <paulo.colombo@pm.me>
2025-03-25 18:01:42 +0100

    test(eth-price): spin up a local forked anvil instance to run price feed tests against

--- packages/eth-price/test/PriceFeedFinder.test.ts
+++ packages/eth-price/test/PriceFeedFinder.test.ts
@@ -1,6 +1,8 @@
 import "./polyfill";
+import { createServer } from "prool";
+import { anvil } from "prool/instances";
 import { createPublicClient, http } from "viem";
-import { mainnet } from "viem/chains";
+import { foundry } from "viem/chains";
 import { expect, describe, it, beforeAll } from "vitest";
 
 import { PriceData, PriceFeedFinder } from "../src/PriceFeedFinder";
@@ -15,11 +17,38 @@ describe("PriceFeedFinder", () => {
   const timestampSeconds = 1737739512n;
 
   beforeAll(async () => {
+    const instance = anvil({
+      forkChainId: 1,
+      forkUrl:
+        process.env.VITEST_MAINNET_FORK_URL ?? "https://eth.llamarpc.com",
+      forkBlockNumber: 22118050,
+      port: 8545,
+    });
+
+    await instance.start();
+
+    const server = createServer({
+      instance,
+    });
+    await server.start();
     const client = createPublicClient({
-      chain: mainnet,
+      chain: {
+        ...foundry,
+        contracts: {
+          multicall3: {
+            address: "0xca11bde05977b3631167028862be2a173976ca11",
+            blockCreated: 14_353_601,
+          },
+        },
+      },
       transport: http(),
     });
     priceFeedFinder = new PriceFeedFinder(client, contractAddress, tolerance);
+
+    return async () => {
+      await instance.stop();
+      await server.stop();
+    };
   });
 
   it("should return the price for a given timestamp", async () => {
