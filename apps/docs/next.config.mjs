c9906c339d1886be0acd8152300d569ffcc18801
Pablo Castellano <pablo@anche.no>
2025-06-23 19:04:23 +0200

    ci(docs): configure output mode in next.config.mjs

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -4,6 +4,7 @@ import withMarkdoc from '@markdoc/next.js'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: process.env.NEXT_BUILD_OUTPUT,
   pageExtensions: ['js', 'jsx', 'md'],
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: !!process.env.CI },
