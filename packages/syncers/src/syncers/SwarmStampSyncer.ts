a3a30be8e1cfe969ab3aef8228423c7f0e400e16
xfja <franjimenezaguilera@gmail.com>
2025-06-27 10:50:08 +0200

    feat(syncers): add support for external batches for Swarm

--- packages/syncers/src/syncers/SwarmStampSyncer.ts
+++ packages/syncers/src/syncers/SwarmStampSyncer.ts
@@ -32,12 +32,12 @@ export class SwarmStampSyncer extends BaseSyncer {
       cronPattern,
       redisUriOrConnection,
       syncerFn: async () => {
-        let response: AxiosResponse<BatchData>;
+        let response: AxiosResponse<BatchData[]>;
 
         try {
-          const url = `${beeEndpoint}/stamps/${batchId}`;
+          const url = `${beeEndpoint}/batches`;
 
-          response = await axios.get<BatchData>(url);
+          response = await axios.get<BatchData[]>(url);
         } catch (err) {
           let cause = err;
 
@@ -50,7 +50,12 @@ export class SwarmStampSyncer extends BaseSyncer {
           });
         }
 
-        const { batchTTL } = response.data;
+        const { batchTTL } =
+          response.data.find((batch) => batch.batchID === batchId) ?? {};
+
+        if (!batchTTL) {
+          throw new Error(`Batch "${batchId}" not found`);
+        }
 
         await prisma.blobStoragesState.upsert({
           create: {
