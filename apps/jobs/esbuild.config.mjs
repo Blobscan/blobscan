15f288e0db4ea9c5408695fb1001f627d488f0d7
PJColombo <paulo.colombo@pm.me>
2025-07-03 16:56:43 +0200

    chore(jobs): add script to build `dist` and metadata

--- apps/jobs/esbuild.config.mjs
+++ apps/jobs/esbuild.config.mjs
@@ -1,17 +1,24 @@
 // esbuild.config.js
 import * as esbuild from "esbuild";
 import glob from "fast-glob";
+import fs from "fs";
 
 const entryPoints = await glob("src/**/worker.ts");
 
-await esbuild.build({
+const result = await esbuild.build({
   entryPoints,
   outdir: "dist",
   outbase: "src",
   platform: "node",
   target: "node20",
   format: "cjs",
+  metafile: !!process.env.BUILD_METADATA_ENABLED,
   bundle: true,
   sourcemap: true,
-  external: ["ioredis", "prisma"],
+  treeShaking: true,
+  external: ["ioredis", "prisma", "@prisma/client"],
 });
+
+if (process.env.BUILD_METADATA_ENABLED) {
+  fs.writeFileSync("dist/meta.json", JSON.stringify(result.metafile));
+}
