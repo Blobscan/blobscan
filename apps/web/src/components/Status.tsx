775a127b546958120c9330cecb3492e1e3e3c0bd
PJColombo <paulo.colombo@pm.me>
2025-07-10 13:22:52 +0200

    fix(web): use app state instead of sync state procedure

--- apps/web/src/components/Status.tsx
+++ apps/web/src/components/Status.tsx
@@ -4,13 +4,15 @@ import { api } from "~/api-client";
 import { StatusBadge } from "./Badges/StatusBadge";
 
 export function BlockStatus({ blockNumber }: { blockNumber: number }) {
-  const { data: syncState } = api.syncState.getState.useQuery();
+  const { data } = api.state.getAppState.useQuery(undefined, {
+    refetchOnWindowFocus: false,
+  });
 
-  if (!syncState || typeof syncState.lastFinalizedBlock !== "number") {
+  if (!data || !data.syncState?.lastFinalizedBlock) {
     return;
   }
 
-  if (blockNumber <= syncState.lastFinalizedBlock) {
+  if (blockNumber <= data.syncState?.lastFinalizedBlock) {
     return (
       <StatusBadge variant="green">
         <CheckCircleIcon className="h-4 w-4" />
