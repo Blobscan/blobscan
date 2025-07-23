1d1a65cdca56501c6b0188cfc96f5a301f4100fd
PJColombo <paulo.colombo@pm.me>
2025-07-23 14:37:44 +0200

    fix(web): remove unused ratios and styles from blockies

--- apps/web/src/components/EthIdenticon.tsx
+++ apps/web/src/components/EthIdenticon.tsx
@@ -1,59 +1,37 @@
 import React from "react";
 import Blockies from "react-blockies";
 
+import type { Size } from "~/types";
 import { isAddress } from "~/utils";
-
-const PX_RATIO = typeof devicePixelRatio === "undefined" ? 2 : devicePixelRatio;
-const BLOCKIES_SQUARES = 8; // commonly used to represent Ethereum addresses
-const BASE_SCALE = 3;
 
 type EthIdenticonProps = {
   address: string;
-  size?: "sm" | "md" | "lg" | "xlg";
+  size?: Size;
   soften?: number;
 };
 
-const SCALES = {
-  sm: 1,
-  md: 2,
-  lg: 3,
-  xlg: 4,
-};
-
-const RADIUS = {
-  sm: "rounded-[3px]",
-  md: "rounded-[5px]",
-  lg: "rounded-[5px]",
-  xlg: "rounded-[5px]",
+const SCALES: Record<Size, number> = {
+  xs: 2,
+  sm: 3,
+  md: 4,
+  lg: 5,
+  xl: 6,
+  "2xl": 7,
 };
 
 export const EthIdenticon: React.FC<EthIdenticonProps> = ({
   address,
   size = "md",
-  soften = 0.3,
 }) => {
-  const blockiesScale = SCALES[size] * BASE_SCALE;
-
   return isAddress(address) ? (
     <div
       className={`
        
-        inline-flex
         overflow-hidden 
         rounded-md
-        align-middle 
       `}
     >
-      <div
-        className={`
-          flex
-          origin-top-left
-          bg-white
-          scale-{${1 / PX_RATIO}}
-        `}
-      >
-        <Blockies seed={address.toLowerCase()} size={6} />
-      </div>
+      <Blockies seed={address} size={8} scale={SCALES[size]} />
     </div>
   ) : null;
 };
