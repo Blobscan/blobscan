b8542d436279980e57c8536e63756fbe98534e89
PJColombo <paulo.colombo@pm.me>
2025-09-20 17:16:26 +0200

    fix(rest-api-server): set `maxRetriesPerRequest` to null on redis client to comply with bullmq

--- apps/rest-api-server/src/clients/redis.ts
+++ apps/rest-api-server/src/clients/redis.ts
@@ -2,7 +2,9 @@ import { Redis } from "ioredis";
 
 import { env } from "@blobscan/env";
 
-const redis = new Redis(env.REDIS_URI);
+const redis = new Redis(env.REDIS_URI, {
+  maxRetriesPerRequest: null,
+});
 
 redis.on("error", (error) => {
   console.error("Redis error:", error);
