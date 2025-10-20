b19e793654a701f7353fc49d22563966602b4351
Pablo Castellano <pablo@anche.no>
2025-10-20 20:56:12 +0200

    feat(api): use logfmt as logging format

--- apps/rest-api-server/src/morgan.ts
+++ apps/rest-api-server/src/morgan.ts
@@ -1,3 +1,4 @@
+import logfmt from "logfmt";
 import morgan from "morgan";
 
 import { logger } from "@blobscan/logger";
@@ -6,4 +7,18 @@ const stream = {
   write: (message: string) => logger.http(message),
 };
 
-export const morganMiddleware = morgan("short", { stream });
+const logfmtFormat = (tokens: any, req: any, res: any) => {
+  const logData = {
+    ip: tokens["remote-addr"](req, res),
+    method: tokens.method(req, res),
+    url: tokens.url(req, res),
+    status: tokens.status(req, res),
+    res_time: `${tokens["response-time"](req, res)}ms`,
+    res_length: tokens.res(req, res, "content-length") || "-",
+    level: "http",
+  };
+
+  return logfmt.stringify(logData);
+};
+
+export const morganMiddleware = morgan(logfmtFormat, { stream });
