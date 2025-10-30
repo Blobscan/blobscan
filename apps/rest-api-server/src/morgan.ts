6909295de5b6e9c46973424f93f7952953d2f1e0
Pablo Castellano <pablo@anche.no>
2025-10-30 13:23:32 +0100

    feat(api): use logfmt as logging format (#909)
    * feat(api): use logfmt as logging format
    
    * feat(api): create specific logfmt logger file
    
    * chore(logger): use logfmt format in general logger
    
    * chore(rest-api-server): revert custom logger
    
    * chore(rest-api-server): format morgan request fields
    
    ---------
    
    Co-authored-by: PJColombo <paulo.colombo@pm.me>

--- apps/rest-api-server/src/morgan.ts
+++ apps/rest-api-server/src/morgan.ts
@@ -3,7 +3,56 @@ import morgan from "morgan";
 import { logger } from "@blobscan/logger";
 
 const stream = {
-  write: (message: string) => logger.http(message),
+  write: (message: string) => {
+    try {
+      const parsed = JSON.parse(message);
+      logger.http("HTTP request handled", parsed);
+    } catch {
+      logger.http(message.trim());
+    }
+  },
 };
 
-export const morganMiddleware = morgan("short", { stream });
+export const morganMiddleware = morgan(
+  (tokens, req, res) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const get = (name: string, ...args: any[]) =>
+      tokens[name]?.(req, res, ...args);
+
+    const data: Record<string, string> = {};
+
+    const ip = get("remote-addr");
+    const method = get("method");
+    const url = get("url");
+    const status = get("status");
+    const responseTime = get("response-time");
+    const contentLength = get("res", "content-length");
+
+    if (ip) {
+      data.ip = ip;
+    }
+
+    if (method) {
+      data.method = method;
+    }
+
+    if (url) {
+      data.url = url;
+    }
+
+    if (status) {
+      data.status = status;
+    }
+
+    if (responseTime) {
+      data.res_time = `${responseTime}ms`;
+    }
+
+    if (contentLength) {
+      data.res_length = contentLength;
+    }
+
+    return JSON.stringify(data);
+  },
+  { stream }
+);
