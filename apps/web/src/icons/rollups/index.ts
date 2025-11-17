bf5bbbdce17edac618167575af9c083ab160210c
PJColombo <paulo.colombo@pm.me>
2025-11-17 20:00:56 -0300

    feat(web): add rollup icons

--- apps/web/src/icons/rollups/index.ts
+++ apps/web/src/icons/rollups/index.ts
@@ -10,17 +10,23 @@ import blast from "./blast.svg";
 import bob from "./bob.svg";
 import boba from "./boba.svg";
 import camp from "./camp.svg";
+import codex from "./codex.svg";
 import debankchain from "./debank.svg";
 import ethernity from "./ethernity.svg";
+import flynet from "./flynet.svg";
 import fraxtal from "./fraxtal.svg";
 import fuel from "./fuel.svg";
+import gravity from "./gravity.svg";
 import hashkey from "./hashkey.svg";
 import hemi from "./hemi.svg";
+import hpp from "./hpp.svg";
 import hypr from "./hypr.svg";
 import ink from "./ink.svg";
 import karak from "./karak.svg";
+import katana from "./katana.svg";
 import kinto from "./kinto.svg";
 import kroma from "./kroma.svg";
+import lighter from "./lighter.svg";
 import linea from "./linea.svg";
 import lisk from "./lisk.svg";
 import mantle from "./mantle.svg";
@@ -39,20 +45,25 @@ import paradex from "./paradex.svg";
 import parallel from "./parallel.svg";
 import pgn from "./pgn.svg";
 import phala from "./phala.svg";
+import plume from "./plume.svg";
 import polynomial from "./polynomial.svg";
+import powerloom from "./powerloom.svg";
 import r0ar from "./r0ar.svg";
 import race from "./race.svg";
 import rari from "./rari.svg";
 import river from "./river.svg";
 import scroll from "./scroll.svg";
+import settlus from "./settlus.svg";
 import shape from "./shape.svg";
 import snaxchain from "./snaxchain.svg";
 import soneium from "./soneium.svg";
+import spire from "./spire.svg";
 import starknet from "./starknet.svg";
 import superlumio from "./superlumio.svg";
 import superseed from "./superseed.svg";
 import swanchain from "./swanchain.svg";
 import swellchain from "./swellchain.svg";
+import symbiosis from "./symbiosis.svg";
 import taiko from "./taiko.svg";
 import thebinaryholdings from "./thebinaryholdings.svg";
 import unichain from "./unichain.svg";
@@ -63,7 +74,7 @@ import zircuit from "./zircuit.svg";
 import zksync from "./zksync.svg";
 import zora from "./zora.svg";
 
-type RollupSvgRegistry = Record<Rollup, RenderableIcon>;
+type RollupSvgRegistry = { [key in Rollup]?: RenderableIcon };
 
 export const ICONS: RollupSvgRegistry = {
   abstract,
@@ -76,17 +87,23 @@ export const ICONS: RollupSvgRegistry = {
   bob,
   boba,
   camp,
+  codex,
   debankchain,
   ethernity,
+  flynet,
   fraxtal,
   fuel,
+  gravity,
   hashkey,
+  hpp,
   hemi,
   hypr,
   ink,
   karak,
+  katana,
   kinto,
   kroma,
+  lighter,
   linea,
   lisk,
   mantle,
@@ -105,20 +122,25 @@ export const ICONS: RollupSvgRegistry = {
   parallel,
   pgn,
   phala,
+  plume,
   polynomial,
+  powerloom,
   r0ar,
   race,
   rari,
   river,
   scroll,
+  settlus,
   shape,
   snaxchain,
   soneium,
+  spire,
   starknet,
   superlumio,
   superseed,
   swanchain,
   swellchain,
+  symbiosis,
   taiko,
   thebinaryholdings,
   unichain,
@@ -131,6 +153,7 @@ export const ICONS: RollupSvgRegistry = {
 
   // PNG-based rollups
   infinaeon: "/rollups/infinaeon.png",
+  lachain: "/rollups/lachain.jpg",
   lambda: "/rollups/lambda.png",
   manta: "/rollups/manta.png",
   orderly: "/rollups/orderly.png",
