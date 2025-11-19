ed3baa04f88ebc19b193ae3962fba95576d86026
PJColombo <paulo.colombo@pm.me>
2025-11-18 21:25:22 -0300

    fix(web): include `.ts` files in tailwind config

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -4,7 +4,7 @@ import baseConfig from "@blobscan/tailwind-config";
 
 export default {
   content: [
-    "./src/**/*.tsx",
+    "./src/**/*.{ts,tsx}",
     "../../node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
   presets: [baseConfig],
