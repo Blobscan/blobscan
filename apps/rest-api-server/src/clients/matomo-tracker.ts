3acf63467262478dc46b5a1cec30b105b86997a7
PJColombo <paulo.colombo@pm.me>
2026-01-15 16:10:59 +0100

    chore(rest-api-server): switch Matomo tracking requests from GET to POST to send auth token in body

--- apps/rest-api-server/src/clients/matomo-tracker.ts
+++ apps/rest-api-server/src/clients/matomo-tracker.ts
@@ -1,7 +1,52 @@
-import MatomoTracker from "matomo-tracker";
-
 import { env } from "@blobscan/env";
+import type { ErrorCause } from "@blobscan/errors";
+import { ErrorException } from "@blobscan/errors";
 import { logger } from "@blobscan/logger";
+
+class MatomoTrackerError extends ErrorException {
+  constructor(message: string, cause: ErrorCause) {
+    super(message, cause);
+  }
+}
+
+interface TrackOptions {
+  url?: string;
+  action_name?: string;
+  ua?: string;
+  cip?: string;
+  cvar?: string;
+  cvar2?: string;
+  lang?: string;
+  pf_srv?: string;
+  token_auth?: string;
+}
+class MatomoTracker {
+  private readonly siteId: string;
+  private readonly url: string;
+
+  constructor(siteId: string | number, url: string) {
+    this.siteId = siteId.toString();
+    this.url = url;
+  }
+
+  async track(options: TrackOptions) {
+    const queryParams = new URLSearchParams({
+      idsite: this.siteId,
+      rec: "1",
+      ...options,
+    });
+
+    try {
+      await fetch(this.url, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: queryParams.toString(),
+      });
+    } catch (error) {
+      throw new MatomoTrackerError("Failed to track event", error as Error);
+    }
+  }
+}
 
 let matomoTracker: MatomoTracker | undefined;
 
