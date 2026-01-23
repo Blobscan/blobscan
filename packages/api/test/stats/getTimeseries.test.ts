fb1a83bd0bee040cb2d074058f40dcf0e5be41f6
PJColombo <paulo.colombo@pm.me>
2026-01-23 17:27:30 +0100

    test(api): fix timeseries procedure UTs

--- packages/api/test/stats/getTimeseries.test.ts
+++ packages/api/test/stats/getTimeseries.test.ts
@@ -1,19 +1,24 @@
 import { beforeAll, beforeEach, describe } from "vitest";
 
+import dayjs from "@blobscan/dayjs";
+import { fixtures } from "@blobscan/test";
+
 import type { TRPCContext } from "../../src";
-import { outputSchema } from "../../src/routers/stats/getDailyStats";
+import { outputSchema } from "../../src/routers/stats/getTimeseries";
 import { createTestContext } from "../helpers";
 import { runTRPCQueryCacheTests } from "../test-suites/cache";
 import type { StatsCaller } from "./caller";
 import { createStatsCaller } from "./caller";
+import { runTimeFrameTests } from "./helpers";
 
-describe("getDailyStats", () => {
+describe("getTimeseriesStats", () => {
   let caller: StatsCaller;
   let ctx: TRPCContext;
-  const input: Parameters<typeof caller.getDailyStats>[0] = {
+  const input: Parameters<typeof caller.getTimeseries>[0] = {
     timeFrame: "7d",
     sort: "desc",
   };
+  const to = dayjs(fixtures.systemDate).endOf("day").toISOString();
 
   beforeAll(async () => {
     ctx = await createTestContext({ withRedis: true });
@@ -29,12 +34,21 @@ describe("getDailyStats", () => {
     };
   });
 
+  runTimeFrameTests({
+    statsFiller() {
+      return ctx.prisma.dailyStats.aggregate({ to });
+    },
+    statsFetcher(timeFrame) {
+      return caller.getTimeseries({ timeFrame });
+    },
+  });
+
   runTRPCQueryCacheTests({
     procedureCall() {
-      return caller.getDailyStats(input);
+      return caller.getTimeseries(input);
     },
     procedureInput: input,
-    procedureName: "getDailyStats",
+    procedureName: "getTimeseries",
     outputSchema: outputSchema,
     ttlProvided: "daily",
   });
