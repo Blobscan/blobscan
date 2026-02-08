de45dd8809633196358f8adff08429b3807dda39
PJColombo <paulo.colombo@pm.me>
2026-02-08 16:41:50 +0100

    feat(web): improve 404 page

--- apps/web/src/pages/404.tsx
+++ apps/web/src/pages/404.tsx
@@ -1,36 +1,40 @@
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Button } from "~/components/Button";
+
 export default function Custom404() {
+  const router = useRouter();
+
   return (
-    <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+    <main className="flex h-[calc(100vh-180px)] w-full flex-col items-center justify-center gap-1 sm:flex-row sm:gap-1">
+      <div className="relative">
+        <div className="absolute right-[20%] top-[70%] h-0.5 w-0.5 animate-pulse bg-yellow-500 shadow-[0px_0px_88px_39px_rgba(255,149,0,1)] sm:shadow-[0px_0px_300px_56px_rgba(255,149,0,1)]" />
+        <Image
+          src="/not-found.png"
+          alt="404"
+          width={500}
+          height={500}
+          className="h-[200px] w-[200px] md:h-[350px] md:w-[350px] lg:h-[450px] lg:w-[450px]"
+        />
+      </div>
       <div className="text-center">
-        <p className="text-base font-semibold text-contentSecondary-light dark:text-contentSecondary-dark">
-          404
-        </p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-content-light dark:text-content-dark sm:text-5xl">
-          Page not found
+        <div className="text-7xl font-bold  md:text-9xl">404</div>
+
+        <h1 className="mt-4 text-3xl tracking-tight text-content-light dark:text-content-dark sm:text-4xl">
+          Page Not Found
         </h1>
 
-        <p className="mt-6 text-base leading-7 dark:text-contentSecondary-dark">
-          Sorry, we couldn’t find the page you’re looking for.
+        <p className="mt-1 text-sm leading-7 text-contentSecondary-light dark:text-contentSecondary-dark sm:mt-6 sm:text-base">
+          Sorry, we couldn&rsquo;t find the page you&rsquo;re looking for.
         </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="/"
-            className={`
-              w-9/12 rounded-md
-              bg-primary-600
-              px-3.5
-              py-2.5 text-sm font-semibold 
-              text-control-light 
-              shadow-sm transition-colors 
-              active:scale-[0.99] 
-              dark:bg-primary-600 
-              dark:text-surfaceContent-dark 
-              dark:hover:bg-accentHighlight-dark
-          `}
-          >
-            Go back home
-          </a>
-        </div>
+        <Button
+          variant="primary"
+          onClick={() => router.replace("/")}
+          className="mt-10 w-48 sm:w-72"
+        >
+          Go back home
+        </Button>
       </div>
     </main>
   );
