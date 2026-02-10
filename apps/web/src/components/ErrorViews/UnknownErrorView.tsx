720d0b393dca4cc28a6074fa92b5224e1055ee39
PJColombo <paulo.colombo@pm.me>
2026-02-10 16:09:40 +0100

    refactor(web): extract error boundary fallback screen to a file

--- apps/web/src/components/ErrorViews/UnknownErrorView.tsx
+++ apps/web/src/components/ErrorViews/UnknownErrorView.tsx
@@ -1,8 +1,10 @@
 import type { FC } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
 
 import { Button } from "../Button";
+import { Icon } from "../Icon";
 import { ErrorViewLayout } from "./ErrorViewLayout";
 
 function GlowingEffect() {
@@ -38,7 +40,10 @@ export const UnknownErrorView: FC<UnknownErrorViewProps> = ({ code }) => {
       description="We ran into an unexpected issue while processing your request."
       action={
         <Button onClick={() => router.reload()} className="w-72">
-          Refresh
+          <div className="flex items-center justify-center gap-1">
+            <Icon src={ArrowPathIcon} size="md" />
+            Reload
+          </div>
         </Button>
       }
     />
