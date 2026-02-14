18ac3dd3e0c86bda60d17b1f4fcc9b6eb42f81ae
PJColombo <paulo.colombo@pm.me>
2026-02-14 13:22:49 +0100

    perf(web): dynamically import rollup SVG icons

--- apps/web/src/components/Badges/RollupBadge.tsx
+++ apps/web/src/components/Badges/RollupBadge.tsx
@@ -21,6 +21,7 @@ export const RollupBadge: React.FC<RollupBadgeProps> = ({
 }) => {
   const { badgeStyle, iconStyle, label } = ROLLUP_STYLES[rollup];
   const rollupIconSrc = ICONS[rollup];
+
   const rollupIcon = rollupIconSrc ? (
     <div className="relative">
       <Icon
