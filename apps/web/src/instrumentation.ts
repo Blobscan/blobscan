0130b8b8ed40fb10d4f6b90a339b5b809d8f540a
PJColombo <paulo.colombo@pm.me>
2026-02-15 13:58:03 +0100

    chore(deps): bump next deps

--- apps/web/src/instrumentation.ts
+++ apps/web/src/instrumentation.ts
@@ -1,6 +1,18 @@
+import { captureRequestError } from "@sentry/nextjs";
+
 // Next.js calls this function when the server starts
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    await import("./instrumentation.node");
+    if (!!process.env.TRACES_ENABLED || !!process.env.METRICS_ENABLED) {
+      await import("./instrumentation.node");
+    }
+
+    await import("./sentry.server.config");
+  }
+
+  if (process.env.NEXT_RUNTIME === "edge") {
+    await import("./sentry.edge.config");
   }
 }
+
+export const onRequestError = captureRequestError;
