282d92c88b455e638d45936d6fcafd78d65e5005
PJColombo <paulo.colombo@pm.me>
2026-02-15 17:35:40 +0100

    chore(docs): fix build process

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,13 +1,15 @@
 import withSearch from './src/markdoc/search.mjs'
 import withMarkdoc from '@markdoc/next.js'
+import { NextConfig } from 'next'
+import path from 'path'
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
+  turbopack: {},
   reactStrictMode: true,
-  output: process.env.NEXT_BUILD_OUTPUT,
+  output: process.env.NEXT_BUILD_OUTPUT as NextConfig['output'],
+  outputFileTracingRoot: path.join(__dirname, '../../'),
   pageExtensions: ['js', 'jsx', 'md'],
   /** We already do linting and typechecking as separate tasks in CI */
-  eslint: { ignoreDuringBuilds: !!process.env.CI },
   typescript: { ignoreBuildErrors: !!process.env.CI },
 }
 
