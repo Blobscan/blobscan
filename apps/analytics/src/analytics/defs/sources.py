186b8e847d59dac2d22519b9a178f28106399d4f
PJColombo <paulo.colombo@pm.me>
2026-02-22 13:15:03 +0100

    feat(analytics): remove unnecessary external assets

--- apps/analytics/src/analytics/defs/sources.py
+++
@@ -1,8 +0,0 @@
-import dagster as dg
-
-transaction = dg.AssetSpec("transaction")
-block = dg.AssetSpec("block")
-address = dg.AssetSpec("address")
-blob = dg.AssetSpec("blob")
-blobs_on_transactions = dg.AssetSpec("blobs_on_transactions")
-transaction_fork = dg.AssetSpec("transaction_fork")
